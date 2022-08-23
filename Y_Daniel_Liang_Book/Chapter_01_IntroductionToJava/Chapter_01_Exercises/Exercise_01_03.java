1227912e8dd08d8441eaaf40e3ca99c552b2218e
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-23 16:51:55 +0200

    Display a pattern

---
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_03.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 23/8/2022
+ * */
+
+package Chapter_01_Exercises;
+
+/*                                                 _Question_
+ * (Display a pattern) Write a program that displays the following pattern :
+ *     J     A    V     V    A
+ *     J    A A    V   V    A A
+ * J   J   AAAAA    V V    AAAAA
+ *  J J   A     A    V    A     A
+ * */
+public class Exercise_01_03 {
+    public static void main(String[] args) {
+        System.out.println("    J     A    V     V    A");
+        System.out.println("    J    A A    V   V    A A");
+        System.out.println("J   J   AAAAA    V V    AAAAA");
+        System.out.println(" J J   A     A    V    A     A");
+    }
+}
+/*                                                 _Output_
+ *     J     A    V     V    A
+ *     J    A A    V   V    A A
+ * J   J   AAAAA    V V    AAAAA
+ *  J J   A     A    V    A     A
+ * */
