262d4a369f9eede376d30a7cd8441551b63774ab
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-23 15:24:34 +0200

    Print Welcome to Java

---
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_01_Welcome.java
@@ -0,0 +1,16 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (Facebook , LinkedIn , Github ) =  @SeifYehiaDev
+ * Created on : 23/8/2022
+ * */
+
+package Chapter_01_Lists;
+
+public class Listing_01_01_Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+    }
+}
+/*                                                 _Output_
+ * Welcome to Java!
+ * */
