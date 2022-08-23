b623ac4d45d9cf43b9b44d3532625fc1210a2374
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-23 16:39:36 +0200

    Display five messages

---
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02.java
@@ -0,0 +1,24 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 23/8/2022
+ * */
+
+package Chapter_01_Exercises;
+
+/*                                                 _Question_
+ * (Display five messages) Write a program that displays Welcome to Java five times.
+ * */
+public class Exercise_01_02 {
+    public static void main(String[] args) {
+        for(int i = 0 ; i<=4 ; i++)
+            System.out.println("Welcome to Java");
+    }
+}
+/*                                                 _Output_
+ * Welcome to Java
+ * Welcome to Java
+ * Welcome to Java
+ * Welcome to Java
+ * Welcome to Java
+ * */
