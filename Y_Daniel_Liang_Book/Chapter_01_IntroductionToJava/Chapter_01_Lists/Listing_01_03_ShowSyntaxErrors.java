548760a60f5e605481cd1ec780236d11368eea11
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-23 16:21:16 +0200

    Show syntax errors

---
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_03_ShowSyntaxErrors.java
@@ -0,0 +1,18 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 23/8/2022
+ * */
+
+package Chapter_01_Lists;
+
+public class Listing_01_03_ShowSyntaxErrors {
+    public static void main(String[] args) {
+        // Syntax Error
+       // System.out.println("Welcome to Java); should be closed with a closing quotation mark
+    }
+}
+
+/*                                                 _Output_
+ *
+ * */
