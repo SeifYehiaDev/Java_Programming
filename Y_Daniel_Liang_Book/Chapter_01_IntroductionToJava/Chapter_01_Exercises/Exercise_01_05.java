fa108e1dcb0b7b0f10a31a57cd9f2a4ed5cc0c71
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 01:43:33 +0200

    Approximate pi

---
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_05.java
@@ -0,0 +1,27 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 23/8/2022
+ * */
+
+package Chapter_01_Exercises;
+
+/*                                                 _Question_
+ * (Compute expressions) Write a program that displays the result of
+ *                            9.5 X 4.5 - 2.5 X 3
+ *                            -------------------
+ *                                45.5 - 3.5
+ * */
+//                                                 _Solution_
+public class Exercise_01_05 {
+    public static void main(String[] args) {
+        System.out.println("9.5 X 4.5 - 2.5 X 3");
+        System.out.println("-------------------- = " + (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+        System.out.println("    45.5 - 3.5     ");
+    }
+}
+/*                                                 _Output_
+ * 9.5 X 4.5 - 2.5 X 3
+ * -------------------- = 0.8392857142857143
+ *     45.5 - 3.5
+ * */
