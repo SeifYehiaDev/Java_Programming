f9f1cc986f77872b704330abef60bc39c7a63dbb
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-23 17:03:48 +0200

    Print a table

---
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_04.java
@@ -0,0 +1,31 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 23/8/2022
+ * */
+
+package Chapter_01_Exercises;
+
+/*                                                 _Question_
+ * (Print a table) Write a program that displays the following table:
+ * a		a^2		a^3
+ * 1		1		1
+ * 2		4		8
+ * 4		16		64
+ * */
+//                                                 _Solution_
+public class Exercise_01_04 {
+    public static void main(String[] args) {
+        System.out.println("a\t\ta^2\t\ta^3");
+        System.out.println("1\t\t1\t\t1");
+        System.out.println("2\t\t4\t\t8");
+        System.out.println("3\t\t9\t\t27");
+        System.out.println("4\t\t16\t\t64");
+    }
+}
+/*                                                 _Output_
+ * a		a^2		a^3
+ * 1		1		1
+ * 2		4		8
+ * 4		16		64
+ * */
