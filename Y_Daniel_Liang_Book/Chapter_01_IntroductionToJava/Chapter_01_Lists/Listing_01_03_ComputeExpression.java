81a88d3c6eb0812ae68e7b40456c25584c194908
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-23 16:10:15 +0200

    Compute expression

---
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_03_ComputeExpression.java
@@ -0,0 +1,17 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 23/8/2022
+ * */
+
+package Chapter_01_Lists;
+
+public class Listing_01_03_ComputeExpression {
+    public static void main(String[] args) {
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+}
+
+/*                                                 _Output_
+ * 0.39759036144578314
+ * */
