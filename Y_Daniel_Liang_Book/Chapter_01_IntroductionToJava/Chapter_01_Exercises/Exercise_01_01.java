386d145b76623ace7b129a0c50757ece984b9a74
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-23 16:36:58 +0200

    Display three messages

--- Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_01.java
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_01.java
@@ -5,14 +5,17 @@
  * */
 
 package Chapter_01_Exercises;
+
 /*                                                 _Question_
  * (Display three messages) Write a program that displays Welcome to Java, Welcome to Computer Science, and Programming is fun.
  * */
 public class Exercise_01_01 {
     public static void main(String[] args) {
-
+        System.out.println("Welcome to Java\n" + "Welcome to Computer Science\n" + "Programming is fun");
     }
 }
 /*                                                 _Output_
- *
+ * Welcome to Java
+ * Welcome to Computer Science
+ * Programming is fun
  * */
