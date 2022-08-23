e41e5d1dfca17eda92d6789670b899811c3aeb44
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-23 15:42:19 +0200

    Compute Area

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Lists/Listing_02_01_ComputeArea.java
@@ -0,0 +1,16 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 23/8/2022
+ * */
+
+package Chapter_02_Lists;
+
+public class Listing_02_01_ComputeArea {
+    public static void main(String[] args) {
+
+    }
+}
+/*                                                 _Output_
+ *
+ * */
