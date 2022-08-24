ba329540c3e9e29669e1adb3d2864b3389d7fcd8
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 12:20:09 +0200

    Which of the following are correct literals for floating-point numbers?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_19.java
@@ -0,0 +1,31 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+import java.util.Scanner;
+
+/*                                                 _Question_
+ * Which of the following are correct literals for floating-point numbers?
+ * 12.3, 12.3e+2, 23.4e-2, –334.4, 20.5, 39F, 40D
+ * */
+//                                                 _Solution_
+public class Question_02_19 {
+    public static void main(String[] args) {
+        /*
+        * 12.3      =======> Double
+        * 12.3E+2   =======> Double
+        * 23.4e-2   =======> Double
+        * -334.5    =======> Double
+        * -20.5     =======> Double
+        * 39F       =======> Float
+        * 40D       =======> Double
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * */
