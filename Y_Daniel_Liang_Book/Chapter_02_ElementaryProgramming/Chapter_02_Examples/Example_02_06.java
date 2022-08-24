664cc6efb5a2bdbffae0388d63cca71b5e656a66
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 12:17:42 +0200

    number multiply 10 power positive or negative number

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Examples/Example_02_06.java
@@ -0,0 +1,31 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Examples;
+
+public class Example_02_06 {
+    public static void main(String[] args) {
+        System.out.println("________________ x * 10 ^ positive number ________________");
+        System.out.println(1.23456E2);
+        System.out.println(1.23456E+2);
+        System.out.println(1.23456e2);
+        System.out.println(1.23456e+2);
+        System.out.println("________________ x * 10 ^ negative number ________________");
+        System.out.println(1.23456E-2);
+        System.out.println(1.23456e-2);
+    }
+}
+
+/*                                                 _Output_
+ * ________________ x * 10 ^ positive number ________________
+ * 123.456
+ * 123.456
+ * 123.456
+ * 123.456
+ * ________________ x * 10 ^ negative number ________________
+ * 0.0123456
+ * 0.0123456
+ * */
