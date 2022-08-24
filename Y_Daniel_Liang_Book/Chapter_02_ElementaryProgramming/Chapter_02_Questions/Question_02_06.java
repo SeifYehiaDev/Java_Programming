aae75db9bb053a1dabb951aa88d1113711ade4c0
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 10:57:51 +0200

    Identify and fix the errors in the following code:

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_06.java
@@ -0,0 +1,27 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * Identify and fix the errors in the following code:
+ * */
+//                                                 _Solution_
+public class Question_02_06 {
+    public static void main(String[] args) {
+        /*
+         * int i = j = k = 2;
+         * System.out.println(i + " " + j + " " + k);
+         * */
+        int i, j, k;
+        i = j = k = 2;
+        System.out.println(i + " " + j + " " + k);
+    }
+}
+
+/*                                                 _Output_
+ * 2 2 2
+ * */
