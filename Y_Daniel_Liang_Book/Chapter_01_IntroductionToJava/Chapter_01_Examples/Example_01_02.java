68f8044f7ca060e4b0e666d02c9e673a1007135a
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 02:16:04 +0200

    Example_01_02

---
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Examples/Example_01_02.java
@@ -0,0 +1,17 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_01_Examples;
+
+public class Example_01_02 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+    }
+}
+
+/*                                                 _Output_
+ * Welcome to Java
+ * */
