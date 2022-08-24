6a244c313d94343a90675a74c8591ae366b151e3
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 12:32:20 +0200

    Which of the following are the same as 52.534?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_20.java
@@ -0,0 +1,34 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+import java.util.Scanner;
+
+/*                                                 _Question_
+ * Which of the following are the same as 52.534?
+ * 5.2534e+1, 0.52534e+2, 525.34e-1, 5.2534e+0
+ * */
+//                                                 _Solution_
+public class Question_02_20 {
+    public static void main(String[] args) {
+        System.out.println(5.2534e+1);
+        System.out.println(0.52534e+2);
+        System.out.println(525.34e-1);
+        System.out.println(5.2534e+0);
+        /*
+        * Which of the following are the same as 52.534?
+        * 5.2534e+1 & 0.52534e+2 & 525.34e-1
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * 52.534
+ * 52.534
+ * 52.534
+ * 5.2534
+ * */
