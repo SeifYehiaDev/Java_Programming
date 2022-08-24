1bbf0780a7b334015ee5dd0b4b7998b567abc4f1
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 10:56:55 +0200

    Are there any performance differences between the following two import statements?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_03.java
@@ -0,0 +1,24 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * Are there any performance differences between the following two import statements?
+ * import java.util.Scanner;
+ * import java.util.*;
+ * */
+//                                                 _Solution_
+public class Question_02_03 {
+    public static void main(String[] args) {
+        // Are there any performance differences between the following two import statements?
+        // import java.util.Scanner; & import java.util.*;
+        // no
+    }
+}
+
+/*                                                 _Output_
+ * */
