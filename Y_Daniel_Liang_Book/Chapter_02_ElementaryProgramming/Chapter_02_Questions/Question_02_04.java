cb004b3bc91206c069abb98b03923f4ed2f31832
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 10:57:11 +0200

    Which of the following identifiers are valid? Which are Java keywords?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_04.java
@@ -0,0 +1,39 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * Which of the following identifiers are valid? Which are Java keywords?
+ * miles, Test, a++, ––a, 4#R, $4, #44, apps , class, public, int, x, y, radius
+ * */
+//                                                 _Solution_
+public class Question_02_04 {
+    public static void main(String[] args) {
+        /*
+        * 01- miles  =  valid
+        * 02- Test   =  valid
+        * 03- a++    = !valid
+        * 04- ––a    = !valid
+        * 05- 4#R    = !valid
+        * 06- $4     =  valid
+        * 07- #44    = !valid
+        * 08- apps   =  valid
+        * 09- class  = !valid
+        * 10- public = !valid
+        * 11- int    = !valid
+        * 12- x      = valid
+        * 13- y      = valid
+        * 14- radius = valid
+        * 15- -seif  = !valid
+        * 16- _Seif  = valid
+        * 17- @Seif  = !valid
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * */
