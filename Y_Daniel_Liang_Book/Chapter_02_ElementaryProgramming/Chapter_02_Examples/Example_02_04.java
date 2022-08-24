16dae93fa871f795072b9e7d3422c819ca4d31e4
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 08:10:29 +0200

    Example_02_04

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Examples/Example_02_04.java
@@ -0,0 +1,51 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Examples;
+
+public class Example_02_04 {
+    public static void main(String[] args) {
+        System.out.println("1 % 5 = " + 1 % 5);
+        System.out.println("2 % 5 = " + 2 % 5);
+        System.out.println("3 % 5 = " + 3 % 5);
+        System.out.println("4 % 5 = " + 4 % 5);
+        System.out.println("5 % 5 = " + 5 % 5);
+        System.out.println("_____________________________________");
+        System.out.println("-7 % 3 = " + -7 % 3);
+        System.out.println("-12 % 4 = " + -12 % 4);
+        System.out.println("-26 % -8 = " + -26 % -8);
+        System.out.println("20 % -13 = " + 20 % -13);
+        System.out.println("______________ odd number % 2 is always 1 ______________");
+        System.out.println("5 % 2 = " + 5 % 2);
+        System.out.println("9 % 2 = " + 9 % 2);
+        System.out.println("231 % 2 = " + 231 % 2);
+        System.out.println("______________ even number % 2 is always 0 ______________");
+        System.out.println("4 % 2 = " + 4 % 2);
+        System.out.println("6 % 2 = " + 6 % 2);
+        System.out.println("8 % 2 = " + 8 % 2);
+    }
+}
+
+/*                                                 _Output_
+ * 1 % 5 = 1
+ * 2 % 5 = 2
+ * 3 % 5 = 3
+ * 4 % 5 = 4
+ * 5 % 5 = 0
+ * _____________________________________
+ * -7 % 3 = -1
+ * -12 % 4 = 0
+ * -26 % -8 = -2
+ * 20 % -13 = 7
+ * ______________ odd number % 2 is always 1 ______________
+ * 5 % 2 = 1
+ * 9 % 2 = 1
+ * 231 % 2 = 1
+ * ______________ even number % 2 is always 0 ______________
+ * 4 % 2 = 0
+ * 6 % 2 = 0
+ * 8 % 2 = 0
+ * */
