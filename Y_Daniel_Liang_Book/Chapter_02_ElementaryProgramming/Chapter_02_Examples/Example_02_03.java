6e2e9b86be0165876538afee6e4b6a54e5912cc6
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 07:23:34 +0200

    Example_02_03

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Examples/Example_02_03.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Examples;
+
+public class Example_02_03 {
+    public static void main(String[] args) {
+        //   int i = j = k = 2; Error
+        // fixed error
+        int i, j, k;
+        i = j = k = 2;
+        System.out.println(i + " " + j + " " + k);
+    }
+}
+
+/*                                                 _Output_
+ * 2 2 2
+ * */
