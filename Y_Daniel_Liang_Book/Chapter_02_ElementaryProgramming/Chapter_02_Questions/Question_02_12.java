6e57e3d580a1fde99cb948821c81b0239282343d
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 09:38:02 +0200

    If today is Tuesday, what will be the day in 100 days?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_12.java
@@ -0,0 +1,46 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * If today is Tuesday, what will be the day in 100 days?
+ * */
+//                                                 _Solution_
+public class Question_02_12 {
+    public static void main(String[] args) {
+        final int TODAY = 2 , DAY = 100; // Mean's today is Tuesday
+        int day = (TODAY + DAY) % 7;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+        }
+    }
+}
+
+/*                                                 _Output_
+ * Thursday
+ * */
