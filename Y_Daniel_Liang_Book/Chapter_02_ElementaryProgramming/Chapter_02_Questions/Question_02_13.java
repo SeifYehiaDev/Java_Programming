0bd9e669a699712a95ce18f606ee9c980ebd9fcc
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 09:43:44 +0200

    What is the result of 25 / 4?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_13.java
@@ -0,0 +1,32 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * What is the result of 25 / 4?
+ * How would you rewrite the expression if you wished the result to be a floating-point number?
+ * */
+//                                                 _Solution_
+public class Question_02_13 {
+    public static void main(String[] args) {
+        System.out.println("25 / 4 = " + 25 / 4);
+        /*
+        * integer / integer = integer
+        * 25 / 4 = 6
+        * */
+        System.out.println("25.0 / 4 = " + 25.0 / 4);
+        /*
+         * double / integer = double
+         * 25.0 / 4 = 6.25
+         * */
+    }
+}
+
+/*                                                 _Output_
+ * 25 / 4 = 6
+ * 25.0 / 4 = 6.25
+ * */
