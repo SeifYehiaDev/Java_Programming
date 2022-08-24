b45a0e4990d2c7b2d07daac672b93336403db484
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 09:55:51 +0200

    Are the following statements correct? If so, show the output.

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_15.java
@@ -0,0 +1,31 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * Are the following statements correct? If so, show the output.
+ * System.out.println("25 / 4 is " + 25 / 4);
+ * System.out.println("25 / 4.0 is " + 25 / 4.0);
+ * System.out.println("3 * 2 / 4 is " + 3 * 2 / 4);
+ * System.out.println("3.0 * 2 / 4 is " + 3.0 * 2 / 4);
+ * */
+//                                                 _Solution_
+public class Question_02_15 {
+    public static void main(String[] args) {
+        System.out.println("25 / 4 is " + 25 / 4); // 6
+        System.out.println("25 / 4.0 is " + 25 / 4.0); // 6.25
+        System.out.println("3 * 2 / 4 is " + 3 * 2 / 4); // 1
+        System.out.println("3.0 * 2 / 4 is " + 3.0 * 2 / 4); // 1.5
+    }
+}
+
+/*                                                 _Output_
+ * 25 / 4 is 6
+ * 25 / 4.0 is 6.25
+ * 3 * 2 / 4 is 1
+ * 3.0 * 2 / 4 is 1.5
+ * */
