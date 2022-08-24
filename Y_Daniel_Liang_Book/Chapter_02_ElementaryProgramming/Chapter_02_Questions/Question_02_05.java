e3a626293edfe9e6bac1bc10cf2f1dcdbf3b82be
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 10:57:36 +0200

    Identify and fix the errors in the following code

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_05.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * Identify and fix the errors in the following code:
+ * */
+//                                                 _Solution_
+public class Question_02_05 {
+    public static void main(String[] args) {
+        /*
+        * int i = k + 2; =======> k is not defined
+        * System.out.println(i);
+         * */
+    }
+}
+
+/*                                                 _Output_
+ * */
