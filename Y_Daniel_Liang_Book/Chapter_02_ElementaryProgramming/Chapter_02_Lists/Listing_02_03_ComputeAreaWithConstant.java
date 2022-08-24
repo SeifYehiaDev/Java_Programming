e600522ac1d6e32fb80851431a6c5604774ee709
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 07:34:50 +0200

    Compute area with constant

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Lists/Listing_02_03_ComputeAreaWithConstant.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Lists;
+
+import java.util.Scanner;
+
+public class Listing_02_03_ComputeAreaWithConstant {
+    public static void main(String[] args) {
+        final double PI = Math.PI;
+        Scanner input = new Scanner(System.in);
+        double radius, area;
+        System.out.print("Enter a number for radius >> ");
+        radius = input.nextDouble();
+        area = Math.pow(radius, 2.0) * PI;
+        System.out.println("The area for the circle of radius " + radius + " is " + area);
+    }
+}
+/*                                                 _Output_
+ * Enter a number for radius >> 5
+ * The area for the circle of radius 5.0 is 78.53981633974483
+ * */
