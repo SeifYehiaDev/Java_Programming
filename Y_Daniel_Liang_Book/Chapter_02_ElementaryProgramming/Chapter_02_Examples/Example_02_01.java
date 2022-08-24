3d88b759b6ecc1653e3c94535b97cb3ceade60fc
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 06:58:46 +0200

    Example_02_01

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Examples/Example_02_01.java
@@ -0,0 +1,20 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Examples;
+
+public class Example_02_01 {
+    public static void main(String[] args) {
+        double i = 50.0;
+        double k = i + 50.0; // k = 100.0
+        double j = k + 1;    // j = 101.0
+        System.out.println("j is " + j + " and k is " + k);
+    }
+}
+
+/*                                                 _Output_
+ * j is 101.0 and k is 100.0
+ * */
