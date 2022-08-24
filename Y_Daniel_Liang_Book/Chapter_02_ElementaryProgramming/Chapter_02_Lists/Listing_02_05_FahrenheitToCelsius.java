e900b98b7d8fe034c0d11418d01fa7eb50119db0
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 12:43:57 +0200

    Fahrenheit to celsius

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Lists/Listing_02_05_FahrenheitToCelsius.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Lists;
+
+import java.util.Scanner;
+
+public class Listing_02_05_FahrenheitToCelsius {
+    public static void main(String[] args) {
+        /*
+         *                 5
+         *   celsius =  ( --- )(fahrenheit - 32).
+         *                 9
+         * */
+        double fahrenheit, celsius;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a degree in Fahrenheit: ");
+        fahrenheit = input.nextDouble();
+        celsius = (5.0 / 9) * (fahrenheit - 32);
+        System.out.println("Fahrenheit " + fahrenheit + " is " + celsius + " in Celsius");
+    }
+}
+/*                                                 _Output_
+ * Enter a degree in Fahrenheit: 100
+ * Fahrenheit 100.0 is 37.77777777777778 in Celsius
+ * */
