5f43fc8bf74f0a9d5d9d83968c48f1322eaf2e13
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 07:19:09 +0200

    Example_02_02

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Examples/Example_02_02.java
@@ -0,0 +1,20 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_02_Examples;
+
+public class Example_02_02 {
+    public static void main(String[] args) {
+        int x = 2 ;
+        System.out.println(x=3);
+        System.out.println(x);
+    }
+}
+
+/*                                                 _Output_
+ * 3
+ * 3
+ * */
