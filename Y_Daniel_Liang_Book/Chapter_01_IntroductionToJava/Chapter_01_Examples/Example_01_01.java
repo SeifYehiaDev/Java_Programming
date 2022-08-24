f76e6af5efdcdc6321e47ba19c1a8daed55fe37e
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-24 02:15:19 +0200

    Example_01_01

---
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Examples/Example_01_01.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 24/8/2022
+ * */
+
+package Chapter_01_Examples;
+
+public class Example_01_01 {
+    public static void main(String[] args) {
+        System.out.println("3.5 * 4 / 2 - 2.5 is ");
+        System.out.println(3.5 * 4 / 2 - 2.5);
+        /*
+        * 3.5 * 4 / 2 - 2.5
+        *  14.0 / 2 - 2.5
+        *  7.0 - 2.5
+        *  4 . 5
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * 3.5 * 4 / 2 - 2.5 is
+ * 4.5
+ * */
