3655314684b0f960f6878b23d9804e4bc1ac7cb0
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-25 07:30:33 +0200

    How do you obtain the current second, minute, and hour?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_23.java
@@ -0,0 +1,30 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 25/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * How do you obtain the current second, minute, and hour?
+ * */
+//                                                 _Solution_
+public class Question_02_23 {
+    public static void main(String[] args) {
+        /*
+        * How do you obtain the current second, minute, and hour?
+        * 1- System.currentTimeMillis();
+        * 2- Obtain the total seconds totalSeconds by dividing totalMilliseconds by 1000
+        * 3- Compute the current second from totalSeconds % 60
+        * 4- Obtain the total minutes totalMinutes by dividing totalSeconds by 60
+        * 5- Compute the current minute from totalMinutes % 60
+        * 6- Obtain the total hours totalHours by dividing totalMinutes by 60
+        * 7- Compute the current hour from totalHours % 24
+        * */
+
+    }
+}
+
+/*                                                 _Output_
+ * */
