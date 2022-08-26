f411346645b5526f6be48d5560971738595c178f
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-26 08:23:15 +0200

    Show the following output

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_29.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 26/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * Show the following output:
+ * */
+//                                                 _Solution_
+public class Question_02_29 {
+    public static void main(String[] args) {
+        float f = 12.5F;
+        int i = (int)f;
+        System.out.println("f is " + f);
+        System.out.println("i is " + i);
+        /*
+        * f = 12.5
+        * i = 12
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * f is 12.5
+ * i is 12
+ * */
