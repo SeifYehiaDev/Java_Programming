33e809b68e99640372d29d2cbce2c557037ebc5b
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-26 08:15:56 +0200

    Can different types of numeric values be used together in a computation?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_27.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 26/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * Can different types of numeric values be used together in a computation?
+ * */
+//                                                 _Solution_
+public class Question_02_27 {
+    public static void main(String[] args) {
+        /*
+        * Can different types of numeric values be used together in a computation?
+        * yes we can by using explicit type conversion
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * */
