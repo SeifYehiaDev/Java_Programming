c4179c067bfee1839649702941b78e16625416b9
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-26 08:20:44 +0200

    What does an explicit casting from a double to an int do with the fractional part of the double value?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_28.java
@@ -0,0 +1,28 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 26/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * What does an explicit casting from a double to an int do with the fractional part of the double value?
+ * Does casting change the variable being cast?
+ * */
+//                                                 _Solution_
+public class Question_02_28 {
+    public static void main(String[] args) {
+        /*
+         * What does an explicit casting from a double to an int do with the fractional part of the double value?
+         * ignore the fraction of double number for example
+         * int x = (int) 5.322323
+         * x = 5
+         * Does casting change the variable being cast?
+         * No change the variable
+         * */
+    }
+}
+
+/*                                                 _Output_
+ * */
