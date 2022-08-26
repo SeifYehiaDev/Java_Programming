1f014e9565d8df7ca513f7ccaef79b7e634dbfb9
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-26 07:44:21 +0200

    Casting conversion

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Examples/Example_02_09.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 26/8/2022
+ * */
+
+package Chapter_02_Examples;
+
+public class Example_02_09 {
+    public static void main(String[] args) {
+        System.out.println("1 / 2 = " + 1 / 2);
+        System.out.println("((double) 1 / 2) == (1.0 / 2) = " + (double) 1 / 2);
+        System.out.println("(double) (1 / 2) = " + (double) (1 / 2));
+    }
+}
+
+/*                                                 _Output_
+ * 1 / 2 = 0
+ * ((double) 1 / 2) == (1.0 / 2) = 0.5
+ * (double) (1 / 2) = 0.0
+ * */
