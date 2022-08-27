fe1802bc9cc5185dffbaeacb8cdc3e324cd35b72
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-27 06:50:36 +0200

    Will overflow cause a runtime error?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_36.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * Will overflow cause a runtime error?
+ * */
+//                                                 _Solution_
+public class Question_02_36 {
+    public static void main(String[] args) {
+        /*
+        * Will overflow cause a runtime error?
+        * no
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * */
