f96ef1256a2e9f7410656a4408b53c1ef75cd21c
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-27 05:45:09 +0200

    Example_02_11

--- Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Examples/Example_02_11.java
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Examples/Example_02_11.java
@@ -1,31 +1,21 @@
 /*
  * Author : Seif Yehia Esmail Abdel-Aal
  * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
- * Created on : 26/8/2022
+ * Created on : 27/8/2022
  * */
 
 package Chapter_02_Examples;
 
 public class Example_02_11 {
     public static void main(String[] args) {
-        int sum = 0;
-        sum += 4.5;
-        System.out.println("sum = " + sum);
-        /*
-         * sum = 0;
-         * sum = sum + 4.5 ;   ===============>!!!Error
-         * sum = 0 + 4.5 = 4.5
-         * int = double
-         * */
-        System.out.println("**************** The same effect as ****************");
-        sum = 0;
-        sum = (int) (sum + 4.5);
-        System.out.println("sum = " + sum);
+        int value = 2147483647 + 1;
+        // int value = 2147483648; Error out of range +1
+        System.out.println("Max value of integer " + Integer.MAX_VALUE + " and minimum value of integer " + Integer.MIN_VALUE);
+        System.out.println("value = " + value);
     }
 }
 
 /*                                                 _Output_
- * sum = 4
- * **************** The same effect as ****************
- * sum = 4
+ * Max value of integer 2147483647 and minimum value of integer -2147483648
+ * value = -2147483648
  * */
