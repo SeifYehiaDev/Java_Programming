fe99535eb531c352d3e4869e80873209dad7592f
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-27 06:49:43 +0200

    Can you declare a variable as int and later redeclare it as double?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_34.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * Can you declare a variable as int and later redeclare it as double?
+ * */
+//                                                 _Solution_
+public class Question_02_34 {
+    public static void main(String[] args) {
+        /*
+        * Can you declare a variable as int and later redeclare it as double?
+        * no
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * */
