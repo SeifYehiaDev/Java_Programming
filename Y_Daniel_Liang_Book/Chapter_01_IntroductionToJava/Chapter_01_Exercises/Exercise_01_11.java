5bb7006b706df8832c0a9edb86f76b2f154d29c4
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-27 11:07:59 +0200

    Population projection

--- Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_11.java
+++ Y_Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_11.java
@@ -1,7 +1,7 @@
 /*
  * Author : Seif Yehia Esmail Abdel-Aal
  * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
- * Created on : 24/8/2022
+ * Created on : 27/8/2022
  * */
 
 package Chapter_01_Exercises;
@@ -22,27 +22,28 @@ package Chapter_01_Exercises;
 //                                                 _Solution_
 public class Exercise_01_11 {
     public static void main(String[] args) {
-        final int CURRENT_POPULATION = 312032486, YEAR_IN_SECOND = 365 * 24 * 60 * 60;
-        final double SECOND_OF_BIRTH = 7.0D, SECOND_OF_DEATH = 13.0D, SECOND_OF_NEW_IMMIGRANT = 45.0D;
-        double populationPerYear = (YEAR_IN_SECOND / SECOND_OF_BIRTH) + (YEAR_IN_SECOND / SECOND_OF_NEW_IMMIGRANT) - (YEAR_IN_SECOND / SECOND_OF_DEATH);
+        final int CURRENT_POPULATION = 312032486, NUMBER_OF_SECOND_IN_YEAR = 31536000;
+        final double SECOND_OF_BIRTH = 7, SECOND_OF_DEATH = 13, SECOND_OF_NEW_IMMIGRANT = 45;
+        double populationPerYear = (NUMBER_OF_SECOND_IN_YEAR / SECOND_OF_BIRTH) - (NUMBER_OF_SECOND_IN_YEAR / SECOND_OF_DEATH) + (NUMBER_OF_SECOND_IN_YEAR / SECOND_OF_NEW_IMMIGRANT);
         System.out.println("Population projection");
         System.out.println("Current population: 312032486");
         System.out.println("One birth every 7 seconds.");
         System.out.println("One death every 13 seconds.");
         System.out.println("One new immigrant every 45 seconds.");
         for (int i = 1; i <= 5; i++)
-            System.out.println("Year " + i + " projection: " + (CURRENT_POPULATION + populationPerYear * i));
+            System.out.println("Year " + i + " projection: " + (int) Math.ceil(CURRENT_POPULATION + populationPerYear * i));
     }
 }
+
 /*                                                 _Output_
  * Population projection
  * Current population: 312032486
  * One birth every 7 seconds.
  * One death every 13 seconds.
  * One new immigrant every 45 seconds.
- * Year 1 projection: 3.148125827032967E8
- * Year 2 projection: 3.175926794065934E8
- * Year 3 projection: 3.203727761098901E8
- * Year 4 projection: 3.231528728131868E8
- * Year 5 projection: 3.2593296951648355E8
+ * Year 1 projection: 314812583
+ * Year 2 projection: 317592680
+ * Year 3 projection: 320372777
+ * Year 4 projection: 323152873
+ * Year 5 projection: 325932970
  * */
