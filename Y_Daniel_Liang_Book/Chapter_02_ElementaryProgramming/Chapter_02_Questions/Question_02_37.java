ffe4ea4f8236dec8c510978a5214366694b0104e
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-27 06:50:58 +0200

    Can floating-point operations cause round-off errors?

---
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_37.java
@@ -0,0 +1,32 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/8/2022
+ * */
+
+package Chapter_02_Questions;
+
+/*                                                 _Question_
+ * What is a round-off error? Can integer operations cause round-off errors?
+ * Can floating-point operations cause round-off errors?
+ * */
+//                                                 _Solution_
+public class Question_02_37 {
+    public static void main(String[] args) {
+        /*
+        * What is a round-off error?
+        * A round-off error, also called a rounding error, is the difference between the calculated approximation
+        * of a number and its exact mathematical value.
+        * System.out.println(1.0 - 0.9);
+        * displays 0.09999999999999998, not 0.1
+        *
+        * Can integer operations cause round-off errors?
+        * no
+        * Can floating-point operations cause round-off errors?
+        * yes
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * */
