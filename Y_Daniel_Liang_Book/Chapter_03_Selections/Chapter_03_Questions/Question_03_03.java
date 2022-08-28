deca34f371df68f12f16f19e6a2ecb6a24e43abd
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-28 20:47:42 +0200

    Can the following conversions involving casting be allowed? Write a test program to verify your answer.

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_03.java
@@ -0,0 +1,38 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 28/8/2022
+ * */
+
+package Chapter_03_Questions;
+
+/*                                                 _Question_
+ * Can the following conversions involving casting be allowed? Write a test program to verify your answer.
+ * boolean b = true;
+ * i = (int)b;
+ *
+ * int i = 1;
+ * boolean b = (boolean)i;
+ * */
+//                                                 _Solution_
+
+public class Question_03_03 {
+    public static void main(String[] args) {
+     /*
+     * Can the following conversions involving casting be allowed?
+     * Not allowed Casting in boolean data type.
+     *
+     * boolean b = true;
+     * i = (int)b;
+     * Error can not cast boolean to integer
+     *
+     * int i = 1;
+     * boolean b = (boolean)i;
+     * Error can not cast integer to boolean
+     * */
+
+    }
+}
+
+/*                                                 _Output_
+ * */
