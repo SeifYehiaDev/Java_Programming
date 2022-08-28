18e4abc2a62b5ac01d9624b6fbbcb7c8bdf769ed
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-28 21:57:17 +0200

    What is the output of the code in (a) and (b) if number is 30? What if number is 35?

--- Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_07.java
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_07.java
@@ -7,16 +7,55 @@
 package Chapter_03_Questions;
 
 /*                                                 _Question_
- *
+ * What is the output of the code in (a) and (b) if number is 30? What if number is 35?
+ * a-                                                        b-
+ * if (number % 2 == 0)                                      if (number % 2 == 0)
+ *  System.out.println(number + " is even.");                 System.out.println(number + " is even.");
+ *                                                           else
+ * System.out.println(number + " is odd.");                  System.out.println(number + " is odd.");
  * */
 //                                                 _Solution_
 
 public class Question_03_07 {
     public static void main(String[] args) {
+        int number = 30;
+        System.out.println("Case a when number = " + number);
+        if (number % 2 == 0)
+            System.out.println(number + " is even.");
 
+        System.out.println(number + " is odd.");
+
+        System.out.println("Case b when number = " + number);
+        if (number % 2 == 0)
+            System.out.println(number + " is even.");
+        else
+            System.out.println(number + " is odd.");
+        System.out.println("_________________________________________");
+        number = 35;
+        System.out.println("Case a when number = " + number);
+        if (number % 2 == 0)
+            System.out.println(number + " is even.");
+
+        System.out.println(number + " is odd.");
+
+        System.out.println("Case b when number = " + number);
+        if (number % 2 == 0)
+            System.out.println(number + " is even.");
+        else
+            System.out.println(number + " is odd.");
+        System.out.println();
     }
 }
 
 /*                                                 _Output_
- *
+ * Case a when number = 30
+ * 30 is even.
+ * 30 is odd.
+ * Case b when number = 30
+ * 30 is even.
+ * _________________________________________
+ * Case a when number = 35
+ * 35 is odd.
+ * Case b when number = 35
+ * 35 is odd.
  * */
