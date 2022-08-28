f128f790311c0c424c5d84651416e3dfe96e5991
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-28 19:58:27 +0200

    Example_03_02

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Examples/Example_03_02.java
@@ -0,0 +1,18 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 28/8/2022
+ * */
+
+package Chapter_03_Examples;
+
+public class Example_03_02 {
+    public static void main(String[] args) {
+        double radius = 1;
+        System.out.println(radius > 0);
+    }
+}
+
+/*                                                 _Output_
+ * true
+ * */
