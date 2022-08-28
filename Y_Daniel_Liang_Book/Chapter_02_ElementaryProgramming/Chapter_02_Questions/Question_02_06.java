7551578bc22afa5bb3efedba05d36215fd8b2333
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-28 18:31:53 +0200

    Identify and fix the errors in the following code:

--- Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_06.java
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_06.java
@@ -14,10 +14,16 @@ public class Question_02_06 {
     public static void main(String[] args) {
         /*
          * int i = j = k = 2;
+         * There's error because j & k not declare.
          * System.out.println(i + " " + j + " " + k);
-         * */
+         * */  //                               Solve this Error
         int i, j, k;
         i = j = k = 2;
+        /*
+        * k = 2;
+        * j = k;
+        * i = j;
+        * */
         System.out.println(i + " " + j + " " + k);
     }
 }
