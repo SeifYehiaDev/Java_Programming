2d6627d8d3f6d06f385a39bd8c1a59735ef74d30
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-28 19:35:27 +0200

    What are the naming conventions for class names, method names, constants, and variables?

--- Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_08.java
+++ Y_Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_08.java
@@ -8,10 +8,38 @@ package Chapter_02_Questions;
 
 /*                                                 _Question_
  * What are the naming conventions for class names, method names, constants, and variables?
+ * Which of the following items can be a constant, a method, a variable, or a class according to the Java naming conventions?
+ * MAX_VALUE, Test, read, readDouble
  * */
 //                                                 _Solution_
 public class Question_02_08 {
     public static void main(String[] args) {
+        /*
+        * What are the naming conventions for class names, method names, constants, and variables?
+        * 1- class names =====> Capitalize the first letter of each word in a class name—for example,
+        * ComputeArea , System . Student
+        *
+        * 2- method names =====> Use lowercase methods.
+        * If a name consists of several words, concatenate them into one, making the first word lowercase
+        * and capitalizing the first letter of each subsequent word—for example,
+        * getRadius , setRadius , sumThreeNumber , displayTheOutput , print
+        *
+        * 3- constants =====> Capitalize every letter in a constant, and use underscores between words—for example,
+        * PI , MAX_SIZE , NUMBER_OF_STUDENTS
+        *
+        * 4- variables =====> Use lowercase methods.
+        * If a name consists of several words, concatenate them into one, making the first word lowercase
+        * and capitalizing the first letter of each subsequent word—for example,
+        * area , size , radius , sum , count , studentName
+         * */
+
+        /*
+        * Which of the following items can be a constant, a method, a variable, or a class according to the Java naming conventions?
+        * 1- ComputeArea =====> class name
+        * 2- Test =====> class name
+        * 3- read =====> method name || variable name
+        * 4- readDouble  =====> method name || variable name
+         * */
     }
 }
 
