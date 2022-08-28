25f5753ffe1221594dfc07605618c4521587f0d4
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-28 22:09:30 +0200

    xxxxxxxxx

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_08.java
@@ -0,0 +1,34 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 28/8/2022
+ * */
+
+package Chapter_03_Questions;
+
+/*                                                 _Question_
+ * Suppose x = 3 and y = 2; show the output,
+ * if any, of the following code.
+ * What is the output if x = 3 and y = 4?
+ * What is the output if x = 2 and y = 2?
+ * Draw a flowchart of the code.
+ *   if (x > 2) {
+            if (y > 2) {
+                z = x + y;
+                System.out.println("z is " + z);
+            }
+        }
+        else
+            System.out.println("x is " + x);
+ * */
+//                                                 _Solution_
+
+public class Question_03_08 {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*                                                 _Output_
+ *
+ * */
