90d49ee89bc45ba61d938af513ccb91849e16c13
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-28 20:22:55 +0200

    List six relational operators?

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_01.java
@@ -0,0 +1,31 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 28/8/2022
+ * */
+
+package Chapter_03_Questions;
+
+/*                                                 _Question_
+ * List six relational operators?
+ * */
+//                                                 _Solution_
+
+public class Question_03_01 {
+    public static void main(String[] args) {
+        /*
+         * List six relational operators?
+         *
+         * 1- >   =================> greater than
+         * 2- >=  =================> greater than or equal to
+         * 3- <   =================> less than
+         * 4- <=  =================> less than or equal to
+         * 5- ==  =================> equal to
+         * 6- !  ==================> not equal to
+         * */
+    }
+}
+
+/*                                                 _Output_
+ *
+ * */
