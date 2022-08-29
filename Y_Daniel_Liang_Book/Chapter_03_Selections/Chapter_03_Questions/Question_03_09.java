84d3672b2c16877a0d1d606b947e6f4055069ae9
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-29 08:44:34 +0200

    Suppose x = 2 and y = 3. Show the output, if any, of the following code.

--- Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_09.java
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_09.java
@@ -1,19 +1,53 @@
 /*
  * Author : Seif Yehia Esmail Abdel-Aal
  * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
- * Created on : 28/8/2022
+ * Created on : 29/8/2022
  * */
 
 package Chapter_03_Questions;
 
 /*                                                 _Question_
- *
+ * Suppose x = 2 and y = 3. Show the output, if any, of the following code.
+ * What is the output if x = 3 and y = 2?
+ * What is the output if x = 3 and y = 3?
+ *       if (x > 2)
+            if (y > 2) {
+                int z = x + y;
+                System.out.println("z is " + z);
+            } else
+                System.out.println("x is " + x);
  * */
 //                                                 _Solution_
 
 public class Question_03_09 {
     public static void main(String[] args) {
+        int x, y;
+        x = 2;
+        y = 3;
+        System.out.println("Case 1 when x = " + x + " & y = " + y);
+        if_else_condition(x, y);
+        // no actions
+        System.out.println("**********************************");
+        x = 3;
+        y = 2;
+        System.out.println("Case 2 when x = " + x + " & y = " + y);
+        if_else_condition(x, y);
+        // x = 3;
+        System.out.println("**********************************");
+        x = 3;
+        y = 3;
+        System.out.println("Case 3 when x = " + x + " & y = " + y);
+        if_else_condition(x, y);
+        // z = 3 + 3 = 6;
+    }
 
+    public static void if_else_condition(int x, int y) {
+        if (x > 2)
+            if (y > 2) {
+                int z = x + y;
+                System.out.println("z is " + z);
+            } else
+                System.out.println("x is " + x);
     }
 }
 
