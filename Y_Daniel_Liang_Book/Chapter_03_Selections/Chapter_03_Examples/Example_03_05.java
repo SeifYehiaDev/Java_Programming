4184cba0c0979c0436b2ffc618098d635659b8d3
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-29 10:16:40 +0200

    Assign boolean in parameter if

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Examples/Example_03_05.java
@@ -0,0 +1,24 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 29/8/2022
+ * */
+
+package Chapter_03_Examples;
+
+public class Example_03_05 {
+    public static void main(String[] args) {
+        int x = 5;
+        boolean isOdd = x % 2 == 1;
+        System.out.println("x = " + x);
+        if (isOdd = false)
+            System.out.println("x is odd number");
+        else
+            System.out.println("x is even number");
+    }
+}
+
+/*                                                 _Output_
+ * x = 5
+ * x is even number
+ * */
