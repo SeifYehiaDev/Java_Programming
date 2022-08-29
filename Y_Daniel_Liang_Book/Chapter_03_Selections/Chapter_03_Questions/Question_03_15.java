1fac5b422da4215ad6939b37c85e51661188f777
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-29 12:57:07 +0200

    Which of the following is a possible output from invoking Math.random()?

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_15.java
@@ -0,0 +1,30 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 29/8/2022
+ * */
+
+package Chapter_03_Questions;
+
+/*                                                 _Question_
+ * Which of the following is a possible output from invoking Math.random()?
+ * 323.4, 0.5, 34, 1.0, 0.0, 0.234
+ * */
+//                                                 _Solution_
+
+public class Question_03_15 {
+    public static void main(String[] args) {
+        /*
+        * Which of the following is a possible output from invoking Math.random()?
+        * 1- 323.4 =====> not correct
+        * 2- 0.5   =====> correct
+        * 3- 34    =====> not correct
+        * 4- 1.0   =====> not correct
+        * 5- 0.0   =====> correct
+        * 6- 0.234 =====> correct
+        * */
+    }
+}
+
+/*                                                 _Output_
+ * */
