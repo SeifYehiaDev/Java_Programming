eb5b816c5dd209ac5de64d7071775800f4c68209
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-29 11:45:32 +0200

    Rewrite the following statement using a Boolean expression

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_12.java
@@ -0,0 +1,44 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 29/8/2022
+ * */
+
+package Chapter_03_Questions;
+
+/*                                                 _Question_
+ * Rewrite the following statement using a Boolean expression:
+ * */
+//                                                 _Solution_
+
+public class Question_03_12 {
+    public static void main(String[] args) {
+        int count = 563;
+        boolean newLine, isYes;
+       /* if (count % 10 == 0)
+            newLine = true;
+        else
+            newLine = false;*/
+
+        // Rewrite the following statement using a Boolean expression:
+        isYes = count % 10 == 0;
+        if (isYes)
+            newLine = true;
+        else
+            newLine = false;
+
+        // inline if
+        isYes = count % 10 == 0;
+        newLine = (isYes) ? true : false;
+
+        // Advanced
+        newLine = false;
+        isYes = count % 10 == 0;
+        if (isYes)
+            newLine = true;
+
+    }
+}
+
+/*                                                 _Output_
+ * */
