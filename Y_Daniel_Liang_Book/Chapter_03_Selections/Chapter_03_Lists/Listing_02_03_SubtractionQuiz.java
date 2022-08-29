4071bd25b48464680ad4fe6786e11cb08bebd3ba
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-29 12:49:41 +0200

    Subtraction quiz

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Lists/Listing_02_03_SubtractionQuiz.java
@@ -0,0 +1,52 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 29/8/2022
+ * */
+
+package Chapter_03_Lists;
+
+import java.util.Scanner;
+
+public class Listing_02_03_SubtractionQuiz {
+    public static void main(String[] args) {
+        int numberOne, numberTwo, answer;
+        Scanner input = new Scanner(System.in);
+        // Generate two random single-digit integers
+        numberOne = (int) (Math.random() * 10);
+        numberTwo = (int) (Math.random() * 10);
+        // If number1 < number2, swap number1 with number2
+        if (numberTwo > numberOne) {
+            int temp = numberOne;
+            numberOne = numberTwo;
+            numberTwo = temp;
+        }
+   /*     if (numberTwo > numberOne) {
+            // swap number1 with number2 without temp
+            numberTwo = numberTwo + numberOne;
+            numberOne = numberTwo - numberOne;
+            numberTwo = numberTwo - numberOne;
+        }*/
+        // Prompt the student to answer ”What is number1 – number2?”
+        System.out.println("What is " + numberOne + " - " + numberTwo + "?");
+        answer = input.nextInt();
+        boolean isAnswer = numberOne - numberTwo == answer;
+        if (isAnswer)
+            System.out.println("you are correct!");
+        else
+            System.out.println("Your answer is Wrong\n" + numberOne + " - " + numberTwo + " should be " + (numberOne - numberTwo));
+    }
+}
+
+/*                                                 _Output One_
+ * What is 9 - 2?
+ * 7
+ * you are correct!
+ * */
+
+/*                                                 _Output Two_
+ * What is 1 - 0?
+ * -5
+ * Your answer is Wrong
+ * 1 - 0 should be 1
+ * */
