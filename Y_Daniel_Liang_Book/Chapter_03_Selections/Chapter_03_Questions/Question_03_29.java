49d67c3e73d8c9b58bb34a75b184d2d334658e46
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-30 12:13:44 +0200

    What data types are required for a switch variable?

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_29.java
@@ -0,0 +1,41 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 30/8/2022
+ * */
+
+package Chapter_03_Questions;
+
+/*                                                 _Question_
+ * What data types are required for a switch variable?
+ * If the keyword break is not used after a case is processed, what is the next statement to be executed?
+ * Can you convert a switch statement to an equivalent if statement, or vice versa?
+ * What are the advantages of using a switch statement?
+ * */
+//                                                 _Solution_
+
+public class Question_03_29 {
+    public static void main(String[] args) {
+        /*
+         * What data types are required for a switch variable?
+         * 1- byte
+         * 2- short
+         * 3- int
+         * 4- character
+         * 5- String
+         *
+         * If the keyword break is not used after a case is processed, what is the next statement to be executed?
+         * executed the next case
+         *
+         * Can you convert a switch statement to an equivalent if statement, or vice versa?
+         * Yes I can.
+         *
+         * What are the advantages of using a switch statement?
+         * Java provides a switch statement to simplify coding for multiple conditions.
+         * You can write the following switch statement to replace the nested if
+         * */
+    }
+}
+
+/*                                                 _Output_
+ * */
---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_30.java
@@ -0,0 +1,4 @@
+package Chapter_03_Questions;
+
+public class Question_03_30 {
+}
