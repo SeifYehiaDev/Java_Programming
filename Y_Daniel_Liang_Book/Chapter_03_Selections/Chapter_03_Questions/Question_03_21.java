6b50466ce5cd46a1971181299348f5e16ec9b2b8
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-30 07:26:37 +0200

    Assume that x and y are int type. Which of the following are legal Java expressions?

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_21.java
@@ -0,0 +1,36 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 30/8/2022
+ * */
+
+package Chapter_03_Questions;
+
+/*                                                 _Question_
+ * Assume that x and y are int type. Which of the following are legal Java expressions?
+ * */
+//                                                 _Solution_
+
+public class Question_03_21 {
+    public static void main(String[] args) {
+        /*
+         * Assume that x and y are int type. Which of the following are legal Java expressions?
+         * 1- x > y > 0; =====> invalid expression because
+         *                               x > y = 2 > 1 = true
+         *                 true > 0 ? not legal to compare boolean value with integer value
+         *
+         * 2 - x = y && y; =====> invalid expression because a & y are integer values.
+         *
+         * 3- x /= y; =====> valid expression
+         *
+         * 4 - x or y; =====> invalid expression because "or" undefined in java but the correct is ||
+         *
+         * 5- x and y =====> invalid expression because "and" undefined in java but the correct is &&
+         *
+         * 6- (x != 0) || (x = 0) =====> invalid expression because (x = 0) not correct the correct is (x == 0)
+         * */
+    }
+}
+
+/*                                                 _Output_
+ * */
