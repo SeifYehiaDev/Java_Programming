c904966654594f2da25f12698b23898e477078a3
SeifYehia <SeifYehiaDev@outlook.com>
2022-08-30 15:02:59 +0200

    True or false? All the binary operators except = are left associative.

---
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_38.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 30/8/2022
+ * */
+
+package Chapter_03_Questions;
+
+/*                                                 _Question_
+ * True or false? All the binary operators except = are left associative.
+ * */
+//                                                 _Solution_
+
+public class Question_03_38 {
+    public static void main(String[] args) {
+        /*
+         * True or false? All the binary operators except = are left associative.
+         * True
+         * All binary operators except assignment operators are left associative.
+         * */
+    }
+}
+
+/*                                                 _Output_
+ * */
