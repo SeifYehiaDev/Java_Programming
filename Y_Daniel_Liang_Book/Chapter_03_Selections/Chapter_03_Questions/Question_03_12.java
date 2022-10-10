fc570ee66273a3e244b9330208bbe2623b95bb23
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-10 07:21:27 +0200

    Question_03_12

--- Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_12.java
+++ Y_Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_12.java
@@ -13,32 +13,14 @@ package Chapter_03_Questions;
 
 public class Question_03_12 {
     public static void main(String[] args) {
-        int count = 563;
-        boolean newLine, isYes;
-       /* if (count % 10 == 0)
+        int count = 100;
+        boolean newLine;
+/*        if (count % 10 == 0)
             newLine = true;
         else
             newLine = false;*/
-
-        // Rewrite the following statement using a Boolean expression:
-        isYes = count % 10 == 0;
-        if (isYes)
-            newLine = true;
-        else
-            newLine = false;
-
-        // inline if
-        isYes = count % 10 == 0;
-        newLine = (isYes) ? true : false;
-
-        // Advanced
-        newLine = false;
-        isYes = count % 10 == 0;
-        if (isYes)
-            newLine = true;
-        // the correct answer
-        newLine = false;
         newLine = count % 10 == 0;
+        System.out.println(newLine);
 
     }
 }
