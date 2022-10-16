02299cfcd18120aac45f3c98885a161c36c01add
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 16:04:24 +0200

    Question_04_15

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_15.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_15.java
@@ -7,15 +7,25 @@
 package Chapter_04_Questions;
 
 //                                                 _Question_
-/**/
+                                /*Show the output of the following statements:*/
 //                                                 _Solution_
 public class Question_04_15 {
     public static void main(String[] args) {
-
+        System.out.println('a' < 'b'); // true
+        System.out.println('a' <= 'A'); // false
+        System.out.println('a' > 'b'); // false
+        System.out.println('a' >= 'A'); // true
+        System.out.println('a' == 'a'); // true
+        System.out.println('a' != 'b'); // true
     }
 }
 
 //                                                 _Output_
 /*
-
+true
+false
+false
+true
+true
+true
  */
