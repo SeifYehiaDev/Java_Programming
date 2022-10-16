b63a276e1d4ae88a8df5a631e5d21d29f6ba832a
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 15:11:15 +0200

    Question_04_08

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_08.java
@@ -0,0 +1,56 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 16/10/2022
+ * */
+
+package Chapter_04_Questions;
+
+//                                                 _Question_
+                /*Use print statements to find out the ASCII code for '1', 'A', 'B', 'a', and 'b'.
+                Use print statements to find out the character for the decimal codes 40, 59, 79, 85,
+                and 90. Use print statements to find out the character for the hexadecimal code 40,
+                5A, 71, 72, and 7A.*/
+//                                                 _Solution_
+public class Question_04_08 {
+    public static void main(String[] args) {
+        System.out.println("ASCII code for '1' is " + (int) '1');
+        System.out.println("ASCII code for 'A' is " + (int) 'A');
+        System.out.println("ASCII code for 'B' is " + (int) 'B');
+        System.out.println("ASCII code for 'a' is " + (int) 'a');
+        System.out.println("ASCII code for 'b' is " + (int) 'b');
+        System.out.println("___________________________________________");
+        System.out.println("Find out the character for the decimal code 40 " + (char) 40);
+        System.out.println("Find out the character for the decimal code 59 " + (char) 59);
+        System.out.println("Find out the character for the decimal code 79 " + (char) 79);
+        System.out.println("Find out the character for the decimal code 85 " + (char) 85);
+        System.out.println("Find out the character for the decimal code 90 " + (char) 90);
+        System.out.println("___________________________________________");
+        System.out.println("Find out the character for the hexadecimal code 40 " + (char) 0x40);
+        System.out.println("Find out the character for the hexadecimal code 5A " + (char) 0x5A);
+        System.out.println("Find out the character for the hexadecimal code 71 " + (char) 0x71);
+        System.out.println("Find out the character for the hexadecimal code 72 " + (char) 0x72);
+        System.out.println("Find out the character for the hexadecimal code 7A " + (char) 0x7A);
+    }
+}
+
+//                                                 _Output_
+/*
+ASCII code for '1' is 49
+ASCII code for 'A' is 65
+ASCII code for 'B' is 66
+ASCII code for 'a' is 97
+ASCII code for 'b' is 98
+___________________________________________
+Find out the character for the decimal code 40 (
+Find out the character for the decimal code 59 ;
+Find out the character for the decimal code 79 O
+Find out the character for the decimal code 85 U
+Find out the character for the decimal code 90 Z
+___________________________________________
+Find out the character for the hexadecimal code 40 @
+Find out the character for the hexadecimal code 5A Z
+Find out the character for the hexadecimal code 71 q
+Find out the character for the hexadecimal code 72 r
+Find out the character for the hexadecimal code 7A z
+ */
---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_13.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 16/10/2022
+ * */
+
+package Chapter_04_Questions;
+
+//                                                 _Question_
+/**/
+//                                                 _Solution_
+public class Question_04_13 {
+    public static void main(String[] args) {
+
+    }
+}
+
+//                                                 _Output_
+/*
+
+ */
