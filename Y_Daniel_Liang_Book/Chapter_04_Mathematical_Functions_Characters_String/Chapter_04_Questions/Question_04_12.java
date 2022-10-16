212f9bef378a98b4ce88845332c6ae4f64881fa1
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 16:02:56 +0200

    Question_04_12

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_12.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_12.java
@@ -7,15 +7,36 @@
 package Chapter_04_Questions;
 
 //                                                 _Question_
-/**/
+        /*Can the following conversions involving casting be allowed? If so, find the converted
+        result.*/
 //                                                 _Solution_
 public class Question_04_12 {
     public static void main(String[] args) {
-
+        char c = 'A';
+        int i = (int) c;
+        System.out.println(i);
+        System.out.println("_________________________");
+        float f = 1000.34f;
+        i = (int) f;
+        System.out.println(i);
+        System.out.println("_________________________");
+        double d = 1000.34;
+        i = (int) d;
+        System.out.println(i);
+        System.out.println("_________________________");
+        i = 97;
+        c = (char) i;
+        System.out.println(c);
     }
 }
 
 //                                                 _Output_
 /*
-
+65
+_________________________
+1000
+_________________________
+1000
+_________________________
+a
  */
