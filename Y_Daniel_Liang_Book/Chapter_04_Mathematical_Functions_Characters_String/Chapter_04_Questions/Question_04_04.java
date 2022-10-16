9bd22372ce1e9d8a4e26b88b38c8fa39c99fa18f
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 11:28:21 +0200

    Question_04_04

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_04.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_04.java
@@ -7,16 +7,22 @@
 package Chapter_04_Questions;
 
 //                                                 _Question_
-/*Write a statement that converts π / 7 to an angle in degrees and assigns the result
-to a variable.*/
+            /*Write a statement that converts π / 7 to an angle in degrees and assigns the result to a variable.*/
 //                                                 _Solution_
 public class Question_04_04 {
     public static void main(String[] args) {
-
+        final double AngleInRadians = Math.PI / 7;
+        double angleInDegree = Math.toDegrees(AngleInRadians);
+        System.out.println("converts π / 7 to an angle in degrees are " + angleInDegree);
+        System.out.println("_________________ At the Same _________________");
+        angleInDegree = AngleInRadians / (Math.PI / 180);
+        System.out.println("converts π / 7 to an angle in degrees are " + angleInDegree);
     }
 }
 
 //                                                 _Output_
 /*
-
+converts π / 7 to an angle in degrees are 25.714285714285715
+_________________ At the Same _________________
+converts π / 7 to an angle in degrees are 25.714285714285715
  */
