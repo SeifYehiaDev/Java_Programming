fb8dcf3679002f5049b8e5f6c3e54edeb22c7566
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 16:03:17 +0200

    Question_04_13

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_13.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_13.java
@@ -7,15 +7,21 @@
 package Chapter_04_Questions;
 
 //                                                 _Question_
-/**/
+                            /*Show the output of the following program:*/
 //                                                 _Solution_
 public class Question_04_13 {
     public static void main(String[] args) {
-
+        char x = 'a';
+        char y = 'c';
+        System.out.println(++x); // b
+        System.out.println(y++); // c
+        System.out.println(x - y); // b - d = -2
     }
 }
 
 //                                                 _Output_
 /*
-
+b
+c
+-2
  */
