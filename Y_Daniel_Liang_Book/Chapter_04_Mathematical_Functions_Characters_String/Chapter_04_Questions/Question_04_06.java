350d595a09cf8ee89d4503bc5dfe38b312201011
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 12:10:21 +0200

    Question_04_06

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_06.java
@@ -0,0 +1,24 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 16/10/2022
+ * */
+
+package Chapter_04_Questions;
+
+//                                                 _Question_
+                        /*Why does the Math class not need to be imported?*/
+//                                                 _Solution_
+public class Question_04_06 {
+    public static void main(String[] args) {
+        /*
+        * Why does the Math class not need to be imported?
+        * because it is in the java.lang package.
+        * All the classes in the java.lang package are implicitly imported in a Java program.
+        * */
+    }
+}
+
+//                                                 _Output_
+/*
+ */
