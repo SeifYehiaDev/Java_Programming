7b5d69a1deea21d6322d1f1a7f2207f245ef5df6
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 11:14:15 +0200

    Question_04_02

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_02.java
@@ -0,0 +1,30 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 16/10/2022
+ * */
+
+package Chapter_04_Questions;
+
+//                                                 _Question_
+/*True or false? The argument for trigonometric methods is an angle in radians.*/
+//                                                 _Solution_
+public class Question_04_02 {
+    public static void main(String[] args) {
+        // True or false? The argument for trigonometric methods is an angle in radians.
+        // True
+        /*
+         * AngleInDegrees = Math.sin(AngleInRadians)
+         * AngleInDegrees = Math.cos(AngleInRadians)
+         * AngleInDegrees = Math.tan(AngleInRadians)
+         *
+         * AngleInRadians = Math.asin(AngleInDegrees)
+         * AngleInRadians = Math.acos(AngleInDegrees)
+         * AngleInRadians = Math.atan(AngleInDegrees)
+         * */
+    }
+}
+
+//                                                 _Output_
+/*
+ */
