21e0eeaacfa803d43699a1c5d9f6cd62da02fb35
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 07:53:25 +0200

    The min, max, and abs Methods

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Examples/Example_04_04.java
@@ -0,0 +1,26 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 16/10/2022
+ * */
+
+package Chapter_04_Examples;
+
+public class Example_04_04 {
+    public static void main(String[] args) {
+        System.out.println("Math.max(2, 3) = " + Math.max(2, 3)); // return 3 ====> integer
+        System.out.println("Math.max(2.5, 3) = " + Math.max(2.5, 3)); // return 3.0 ====> double
+        System.out.println("Math.min(2.5, 4.6) = " + Math.min(2.5, 4.6)); // return 2.5 ====> double
+        System.out.println("Math.abs(-2) = " + Math.abs(-2)); // return 2 ====> integer
+        System.out.println("Math.abs(-2.1) = " + Math.abs(-2.1)); // return 2.1 ====> integer
+    }
+}
+
+//                                                 _Output_
+/*
+Math.max(2, 3) = 3
+Math.max(2.5, 3) = 3.0
+Math.min(2.5, 4.6) = 2.5
+Math.abs(-2) = 2
+Math.abs(-2.1) = 2.1
+*/
