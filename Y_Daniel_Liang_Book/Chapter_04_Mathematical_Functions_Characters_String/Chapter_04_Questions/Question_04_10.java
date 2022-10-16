cf32c960691f05e123e00adf8483d5eae39b9762
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 15:28:57 +0200

    Question_04_10

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_10.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_10.java
@@ -7,15 +7,17 @@
 package Chapter_04_Questions;
 
 //                                                 _Question_
-/**/
+                            /*How do you display the characters \ and "? */
 //                                                 _Solution_
 public class Question_04_10 {
     public static void main(String[] args) {
-
+        System.out.println("\\");
+        System.out.println("\"");
     }
 }
 
 //                                                 _Output_
 /*
-
+\
+"
  */
