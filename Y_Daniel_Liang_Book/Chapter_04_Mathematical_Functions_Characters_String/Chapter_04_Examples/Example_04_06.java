a3ddf8733e08422b3946d80a2799c2238ac5ce12
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-16 13:11:16 +0200

    Unicode

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Examples/Example_04_06.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 16/10/2022
+ * */
+
+package Chapter_04_Examples;
+
+public class Example_04_06 {
+    public static void main(String[] args) {
+        char charA = 'A';
+        System.out.println(charA);
+        System.out.println("________________ At the same ________________");
+        char UniCodeOfA = '\u0041';
+        System.out.println(UniCodeOfA);
+        System.out.println("_______________________________________________");
+        char chr = 'a';
+        System.out.println(++chr);
+    }
+}
+
+//                                                 _Output_
+/*
+A
+________________ At the same ________________
+A
+_______________________________________________
+b
+*/
