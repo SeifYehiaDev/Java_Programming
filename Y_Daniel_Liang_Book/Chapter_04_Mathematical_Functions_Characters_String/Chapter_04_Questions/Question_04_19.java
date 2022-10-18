74ff1e76468fe5d0a8d6de56f7c7616522d05f9b
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-18 11:14:24 +0200

    Question_04_19

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_19.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_19.java
@@ -14,8 +14,8 @@ public class Question_04_19 {
         System.out.println(1 + "Welcome " + 1 + 1); // 1Welcome 11
         System.out.println(1 + "Welcome " + (1 + 1)); // 1Welcome 2
         System.out.println(1 + "Welcome " + ('\u0001' + 1));// 1Welcome 2
-        System.out.println(1 + "Welcome " + ('a' + 1)); //1Welcome 98
-        System.out.println(1 + "Welcome " + 'a' + 1); //1Welcome a1
+        System.out.println(1 + "Welcome " + ('a' + 1)); // 1Welcome 98
+        System.out.println(1 + "Welcome " + 'a' + 1); // 1Welcome a1
     }
 }
 
