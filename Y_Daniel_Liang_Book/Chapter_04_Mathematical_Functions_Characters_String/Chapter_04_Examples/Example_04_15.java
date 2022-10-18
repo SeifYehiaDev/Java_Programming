62bf7a91813581ed09b60bdb96044ba5b1fa700a
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-18 06:43:11 +0200

    Problem in Comparing Strings

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Examples/Example_04_15.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Examples/Example_04_15.java
@@ -6,9 +6,20 @@
 
 package Chapter_04_Examples;
 
+import java.util.Scanner;
+
 public class Example_04_15 {
     public static void main(String[] args) {
-
+        String firstString = "Hello", secondString;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the second string to compare: ");
+        secondString = input.next();
+        System.out.println("The first string is ".concat(firstString));
+        System.out.println("The second string is ".concat(secondString));
+        if (firstString == secondString)
+            System.out.println("string1 and string2 are the same");
+        else
+            System.out.println("string1 and string2 are different");
     }
 }
 
