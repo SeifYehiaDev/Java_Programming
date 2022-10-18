8fd83e9023f82972cb0ccace9d053aa7c91ec990
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-18 11:06:42 +0200

    Question_04_18

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_18.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_18.java
@@ -7,15 +7,23 @@
 package Chapter_04_Questions;
 
 //                                                 _Question_
-/**/
+            /*Show the output of the following statements (write a program to verify your results):*/
 //                                                 _Solution_
 public class Question_04_18 {
     public static void main(String[] args) {
-
+        System.out.println("1" + 1); // 11
+        System.out.println('1' + 1); // 49 + 1 = 50
+        System.out.println("1" + 1 + 1); // 111
+        System.out.println("1" + (1 + 1)); // 12
+        System.out.println('1' + 1 + 1); // 49 + 1 + 1 = 51
     }
 }
 
 //                                                 _Output_
 /*
-
+11
+50
+111
+12
+51
  */
