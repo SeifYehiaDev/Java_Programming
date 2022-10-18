335552f96183afb560cea7ba9750c1ae2776fb54
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-18 05:02:19 +0200

    The String Type

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Examples/Example_04_11.java
@@ -0,0 +1,26 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 18/10/2022
+ * */
+
+package Chapter_04_Examples;
+
+public class Example_04_11 {
+    public static void main(String[] args) {
+        String message = "Welcome to Java";
+        System.out.println(message);
+        /*
+         * String: String is a predefined class in the Java library, just like the classes System and Scanner.
+         * The String type is not a primitive type. It is known as a reference type.
+         *
+         * The variable declared by a reference type is known as a reference variable that references an object.
+         * Here, message is a reference variable that references a string object with contents Welcome to Java.
+         * */
+    }
+}
+
+//                                                 _Output_
+/*
+Welcome to Java
+ */
