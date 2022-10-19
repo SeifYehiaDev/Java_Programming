db1810a38bc7515ac69a1a8e9a5a24e978b9cb50
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-19 12:47:30 +0200

    Question_04_22

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_22.java
@@ -0,0 +1,32 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 19/10/2022
+ * */
+
+package Chapter_04_Questions;
+
+//                                                 _Question_
+            /*What are the format specifiers for outputting a Boolean value, a character, a decimal
+            integer, a floating-point number, and a string?*/
+//                                                 _Solution_
+public class Question_04_22 {
+    public static void main(String[] args) {
+        System.out.printf("What are the format specifiers for outputting a Boolean value, a character, a decimal integer, a floating-point number, and a string?\n");
+        System.out.printf("Boolean value = %%b\n");
+        System.out.printf("character value = %%c\n");
+        System.out.printf("decimal integer = %%d\n");
+        System.out.printf("floating-point number = %%f\n");
+        System.out.printf("string = %%s\n");
+    }
+}
+
+//                                                 _Output_
+/*
+What are the format specifiers for outputting a Boolean value, a character, a decimal integer, a floating-point number, and a string?
+Boolean value = %b
+character value = %c
+decimal integer = %d
+floating-point number = %f
+string = %s
+ */
