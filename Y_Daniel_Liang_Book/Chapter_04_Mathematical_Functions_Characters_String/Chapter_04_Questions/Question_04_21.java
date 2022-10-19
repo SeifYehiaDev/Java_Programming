07c1e9ede995de39a1add6c3f51179bdeeb38190
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-19 08:08:42 +0200

    Question_04_21

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_21.java
@@ -0,0 +1,22 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 19/10/2022
+ * */
+
+package Chapter_04_Questions;
+
+//                                                 _Question_
+                /*If you run Listing 4.3 GuessBirthday.java with input 1 for Set1, Set3, and Set4 and 0
+                for Set2 and Set5, what will be the birthday?*/
+//                                                 _Solution_
+public class Question_04_21 {
+    public static void main(String[] args) {
+        System.out.println("Your birthday is " + (1 + 0 + 4 + 8 + 0) + "!");
+    }
+}
+
+//                                                 _Output_
+/*
+Your birthday is 13!
+ */
