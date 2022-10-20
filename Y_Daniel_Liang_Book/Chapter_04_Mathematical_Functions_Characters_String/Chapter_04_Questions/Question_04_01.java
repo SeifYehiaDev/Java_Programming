cbd05720597c7a05bde02a0cdb11cc7fb4c0fa41
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-20 08:10:54 +0200

    Question_04_01

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_01.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Questions/Question_04_01.java
@@ -17,18 +17,18 @@ public class Question_04_01 {
         System.out.println("(d) Math.pow(2, 2) = " + Math.pow(2, 2)); // return 4.0
         System.out.println("(e) Math.log(Math.E) = " + Math.log(Math.E)); // return 1.0
         System.out.println("(f) Math.exp(1) = " + Math.exp(1)); // return Math.E = 2.718281828459045
-        System.out.println("(g) Math.max(2, Math.min(3, 4)) = " + Math.max(2, Math.min(3, 4))); // return 3.0
+        System.out.println("(g) Math.max(2, Math.min(3, 4)) = " + Math.max(2, Math.min(3, 4))+" ===> int"); // return 3
         System.out.println("(h) Math.rint(-2.5) = " + Math.rint(-2.5)); // return -2.0
         System.out.println("(i) Math.ceil(-2.5) = " + Math.ceil(-2.5)); // return -2.0
         System.out.println("(j) Math.floor(-2.5) = " + Math.floor(-2.5)); // return -3.0
-        System.out.println("(k) Math.round(-2.5f) = " + Math.round(-2.5f)); // return -2 ===> int
-        System.out.println("(l) Math.round(-2.5) = " + Math.round(-2.5)); // return -2 ===> long
+        System.out.println("(k) Math.round(-2.5f) = " + Math.round(-2.5f)+" ===> int"); // return -2 ===> int
+        System.out.println("(l) Math.round(-2.5) = " + Math.round(-2.5)+" ===> long"); // return -2 ===> long
         System.out.println("(m) Math.rint(2.5) = " + Math.rint(2.5)); // return 2.0
         System.out.println("(n) Math.ceil(2.5) = " + Math.ceil(2.5)); // return 3.0
         System.out.println("(o) Math.floor(2.5) = " + Math.floor(2.5)); // return 2.0
-        System.out.println("(p) Math.round(2.5f) = " + Math.round(2.5f)); // return 3 ===> int
-        System.out.println("(q) Math.round(2.5) = " + Math.round(2.5)); // return 3 ===> long
-        System.out.println("(r) Math.round(Math.abs(-2.5)) = " + Math.round(Math.abs(-2.5))); // return 3 ===> long
+        System.out.println("(p) Math.round(2.5f) = " + Math.round(2.5f)+" ===> int"); // return 3 ===> int
+        System.out.println("(q) Math.round(2.5) = " + Math.round(2.5)+" ===> long"); // return 3 ===> long
+        System.out.println("(r) Math.round(Math.abs(-2.5)) = " + Math.round(Math.abs(-2.5))+" ===> long"); // return 3 ===> long
     }
 }
 
@@ -40,16 +40,16 @@ public class Question_04_01 {
 (d) Math.pow(2, 2) = 4.0
 (e) Math.log(Math.E) = 1.0
 (f) Math.exp(1) = 2.718281828459045
-(g) Math.max(2, Math.min(3, 4)) = 3
+(g) Math.max(2, Math.min(3, 4)) = 3 ===> int
 (h) Math.rint(-2.5) = -2.0
 (i) Math.ceil(-2.5) = -2.0
 (j) Math.floor(-2.5) = -3.0
-(k) Math.round(-2.5f) = -2
-(l) Math.round(-2.5) = -2
+(k) Math.round(-2.5f) = -2 ===> int
+(l) Math.round(-2.5) = -2 ===> long
 (m) Math.rint(2.5) = 2.0
 (n) Math.ceil(2.5) = 3.0
 (o) Math.floor(2.5) = 2.0
-(p) Math.round(2.5f) = 3
-(q) Math.round(2.5) = 3
-(r) Math.round(Math.abs(-2.5)) = 3
+(p) Math.round(2.5f) = 3 ===> int
+(q) Math.round(2.5) = 3 ===> long
+(r) Math.round(Math.abs(-2.5)) = 3 ===> long
 */
