7f7aec7cfdd3ab8969552d5cbcbb9b0996c8c0ce
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-20 11:09:50 +0200

    Geography: estimate areas

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_03.java
@@ -0,0 +1,26 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 20/10/2022
+ * */
+
+package Chapter_04_Exercises;
+
+
+//                                                 _Question_
+            /*(Geography: estimate areas) Find the GPS locations for Atlanta, Georgia;
+            Orlando, Florida; Savannah, Georgia; and Charlotte, North Carolina from
+            www.gps-data-team.com/map/ and compute the estimated area enclosed by these
+            four cities. (Hint: Use the formula in Programming Exercise 4.2 to compute the
+            distance between two cities. Divide the polygon into two triangles and use the
+            formula in Programming Exercise 2.19 to compute the area of a triangle.)*/
+//                                                 _Solution_
+public class Exercise_04_03 {
+    public static void main(String[] args) {
+
+    }
+}
+
+//                                                 _Output_
+/*
+ */
