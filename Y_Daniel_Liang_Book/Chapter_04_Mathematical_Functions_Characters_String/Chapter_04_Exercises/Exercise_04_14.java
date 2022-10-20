e768e60fdabf3812b3fdb09aee4efc5edea5cb0d
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-20 18:52:52 +0200

    Convert letter grade to number

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_14.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_14.java
@@ -7,12 +7,34 @@
 package Chapter_04_Exercises;
 
 
+import java.util.Scanner;
+
 //                                                 _Question_
-/**/
+                /*(Convert letter grade to number) Write a program that prompts the user to enter a
+                letter grade A, B, C, D, or F and displays its corresponding numeric value 4, 3, 2,
+                1, or 0.*/
 //                                                 _Solution_
 public class Exercise_04_14 {
     public static void main(String[] args) {
-
+        String stringGrade;
+        char charGrade;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a letter grade: ");
+        stringGrade = input.nextLine();
+        charGrade = Character.toUpperCase(stringGrade.charAt(0));
+        if (stringGrade.length() != 1 || charGrade < 'A' || charGrade > 'F') {
+            System.out.println((stringGrade.length() != 1) ? "You must enter exactly one character" : stringGrade.charAt(0) + " is an invalid grade");
+            System.exit(1);
+        }
+        switch (charGrade) {
+            case 'A':
+                stringGrade = "4";break;
+            case 'B': stringGrade = "3";break;
+            case 'C': stringGrade = "2";break;
+            case 'D': stringGrade = "1";break;
+            default: stringGrade = "0";
+        }
+        System.out.println("The corresponding number is ".concat(stringGrade));
     }
 }
 
