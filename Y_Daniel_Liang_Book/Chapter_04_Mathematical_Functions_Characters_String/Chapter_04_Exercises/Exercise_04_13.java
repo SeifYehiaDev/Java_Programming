7148bcf4184ee5194c9dca52be882073cf136208
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-20 17:43:45 +0200

    Vowel or consonant?

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_13.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_13.java
@@ -6,17 +6,41 @@
 
 package Chapter_04_Exercises;
 
+import java.util.Scanner;
 
 //                                                 _Question_
-/**/
+                    /*(Vowel or consonant?) Write a program that prompts the user to enter a letter and
+                    check whether the letter is a vowel or consonant.*/
 //                                                 _Solution_
 public class Exercise_04_13 {
     public static void main(String[] args) {
-
+        char letter, upperLetter;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a letter: ");
+        letter = input.nextLine().charAt(0);
+        if (!Character.isLetter(letter)) {
+            System.out.println(letter + " is an invalid input");
+            System.exit(1);
+        }
+        upperLetter = Character.toUpperCase(letter);
+        System.out.println(letter + " is a " + ((upperLetter == 'A' || upperLetter == 'O' || upperLetter == 'U' || upperLetter == 'I' || upperLetter == 'E') ? "vowel" : "consonant"));
     }
 }
 
-//                                                 _Output_
+//                                                 _Output One_
 /*
+Enter a letter: B
+B is a consonant
+ */
 
+//                                                 _Output Two_
+/*
+Enter a letter: a
+a is a vowel
+ */
+
+//                                                 _Output Three_
+/*
+Enter a letter: #
+# is an invalid input
  */
