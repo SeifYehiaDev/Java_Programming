c19281f126c6fc12febd9683f73d1ce79306d8a3
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-20 14:41:15 +0200

    Corner point coordinates

---
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_00.java
@@ -0,0 +1,26 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 20/10/2022
+ * */
+
+package Chapter_04_Exercises;
+
+
+//                                                 _Question_
+/**/
+//                                                 _Solution_
+public class Exercise_04_00 {
+    public static void main(String[] args) {
+        System.out.println((char) 40);
+        System.out.println((char) 59);
+        System.out.println((char) 79);
+        System.out.println((char) 85);
+        System.out.println((char) 90);
+    }
+}
+
+//                                                 _Output_
+/*
+
+ */
