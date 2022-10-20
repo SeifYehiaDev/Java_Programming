bd082ee6771b98e0f35d4a8288337aa8dd112c92
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-20 15:07:41 +0200

    Find the Unicode of a character

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_09.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_09.java
@@ -6,17 +6,24 @@
 
 package Chapter_04_Exercises;
 
+import java.util.Scanner;
 
 //                                                 _Question_
-/**/
+                    /*(Find the Unicode of a character) Write a program that receives a character and
+                    displays its Unicode.*/
 //                                                 _Solution_
 public class Exercise_04_09 {
     public static void main(String[] args) {
-
+        char chr;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        chr = input.nextLine().charAt(0);
+        System.out.println("The Unicode for the character " + chr + " is " + (int) chr);
     }
 }
 
 //                                                 _Output_
 /*
-
+Enter a character: E
+The Unicode for the character E is 69
  */
