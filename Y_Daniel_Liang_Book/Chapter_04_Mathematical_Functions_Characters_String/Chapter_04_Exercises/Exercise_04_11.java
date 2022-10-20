88cf159aa9aceae1f3d48daf6deb58e9f52b022f
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-20 16:38:07 +0200

    Decimal to hex

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_11.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_11.java
@@ -6,17 +6,36 @@
 
 package Chapter_04_Exercises;
 
+import java.util.Scanner;
 
 //                                                 _Question_
-/**/
+                /*(Decimal to hex) Write a program that prompts the user to enter an integer between
+                0 and 15 and displays its corresponding hex number.*/
 //                                                 _Solution_
 public class Exercise_04_11 {
     public static void main(String[] args) {
-
+        int hexadecimal;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a decimal value (0 to 15): ");
+        hexadecimal = input.nextInt();
+        System.out.println((hexadecimal >= 0 && hexadecimal <= 9) ? "The hex value is " + hexadecimal : (hexadecimal >= 9 && hexadecimal <= 15) ? "The hex value is " + (char) (hexadecimal - 10 + 'A') : hexadecimal + " is an invalid input");
     }
 }
 
-//                                                 _Output_
+//                                                 _Output One_
 /*
+Enter a decimal value (0 to 15): 11
+The hex value is B
+ */
 
+//                                                 _Output Two_
+/*
+Enter a decimal value (0 to 15): 5
+The hex value is 5
+ */
+
+//                                                 _Output Three_
+/*
+Enter a decimal value (0 to 15): 35
+35 is an invalid input
  */
