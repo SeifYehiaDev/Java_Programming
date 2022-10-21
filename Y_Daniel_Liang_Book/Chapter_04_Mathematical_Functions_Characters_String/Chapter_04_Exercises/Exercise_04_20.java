401584a7aab29aff8afb92ac419b08e459a3fb47
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-21 11:21:36 +0200

    Process a string

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_20.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_20.java
@@ -1,22 +1,31 @@
 /*
  * Author : Seif Yehia Esmail Abdel-Aal
  * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
- * Created on : 20/10/2022
+ * Created on : 21/10/2022
  * */
 
 package Chapter_04_Exercises;
 
+import java.util.Scanner;
 
 //                                                 _Question_
-/**/
+                /*(Process a string) Write a program that prompts the user to enter a string and displays
+                its length and its first character.*/
 //                                                 _Solution_
 public class Exercise_04_20 {
     public static void main(String[] args) {
-
+        String message;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your message: ");
+        message = input.nextLine();
+        System.out.println("The message length is " + message.length());
+        System.out.println("The first character of your message is '" + message.charAt(0) + "'");
     }
 }
 
 //                                                 _Output_
 /*
-
+Enter your message: Welcome to Java
+The message length is 15
+The first character of your message is 'W'
  */
