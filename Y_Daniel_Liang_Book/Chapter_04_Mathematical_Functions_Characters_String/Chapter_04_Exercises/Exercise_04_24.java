8031b4499437418fd508c367642f4c6f545f16d5
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-27 23:02:50 +0200

    Order three cities

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_24.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_24.java
@@ -1,22 +1,37 @@
 /*
  * Author : Seif Yehia Esmail Abdel-Aal
  * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
- * Created on : 20/10/2022
+ * Created on : 27/10/2022
  * */
 
 package Chapter_04_Exercises;
 
+import java.util.Arrays;
+import java.util.Scanner;
 
 //                                                 _Question_
-/**/
+            /*(Order three cities) Write a program that prompts the user to enter three cities and
+            displays them in ascending order.*/
 //                                                 _Solution_
 public class Exercise_04_24 {
     public static void main(String[] args) {
-
+        String[] cities = new String[3];
+        Scanner input = new Scanner(System.in);
+        for (int index = 0; index < cities.length; index++) {
+            System.out.print("Enter the ".concat((index == 0) ? "first" : (index == 1) ? "second" : "third").concat(" city: "));
+            cities[index] = input.nextLine();
+        }
+        Arrays.sort(cities);
+        System.out.print("The three cities in alphabetical order are ");
+        for (String city : cities)
+            System.out.print(city + " ");
     }
 }
 
 //                                                 _Output_
 /*
-
+Enter the first city: Chicago
+Enter the second city: Los Angeles
+Enter the third city: Atlanta
+The three cities in alphabetical order are Atlanta Chicago Los Angeles
  */
