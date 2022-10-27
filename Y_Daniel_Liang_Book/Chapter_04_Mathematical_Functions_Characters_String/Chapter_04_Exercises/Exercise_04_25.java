b974c250397311a617c61cef9a7859a75c682c8d
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-27 23:16:44 +0200

    Generate vehicle plate numbers

--- Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_25.java
+++ Y_Daniel_Liang_Book/Chapter_04_Mathematical_Functions_Characters_String/Chapter_04_Exercises/Exercise_04_25.java
@@ -1,22 +1,24 @@
 /*
  * Author : Seif Yehia Esmail Abdel-Aal
  * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
- * Created on : 20/10/2022
+ * Created on : 27/10/2022
  * */
 
 package Chapter_04_Exercises;
 
 
 //                                                 _Question_
-/**/
+            /*(Generate vehicle plate numbers) Assume a vehicle plate number consists of three
+            uppercase letters followed by four digits. Write a program to generate a plate
+            number.*/
 //                                                 _Solution_
 public class Exercise_04_25 {
     public static void main(String[] args) {
-
+        System.out.println((char) (Math.random() * ('Z' - 'A' + 1) + 'A') + "" + (char) (Math.random() * ('Z' - 'A' + 1) + 'A') + (char) (Math.random() * ('Z' - 'A' + 1) + 'A') + (int) (Math.random() * (9999 - 1000 + 1) + 1000));
     }
 }
 
 //                                                 _Output_
 /*
-
+XQI4584
  */
