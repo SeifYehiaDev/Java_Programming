f271c7771bf4f8df8fdac11ae50c9574859322c1
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-28 15:40:18 +0200

    Repeat addition quiz

--- Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Lists/Listing_05_01_RepeatAdditionQuiz.java
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Lists/Listing_05_01_RepeatAdditionQuiz.java
@@ -11,29 +11,23 @@ import java.util.Scanner;
 public class Listing_05_01_RepeatAdditionQuiz {
     public static void main(String[] args) {
         int randomNumbers[] = new int[2], answer;
-        boolean validAnswer;
+        boolean validAnswer = true;
         Scanner input = new Scanner(System.in);
         for (int index = 0; index < randomNumbers.length; index++)
             randomNumbers[index] = (int) (Math.random() * 10);
         do {
-            System.out.print("What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? ");
+            System.out.print((validAnswer) ? "What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? " : "Wrong answer. Try again. What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? ");
             answer = input.nextInt();
             validAnswer = randomNumbers[0] + randomNumbers[1] == answer;
-            if(!validAnswer)
-                System.out.print("Wrong answer. Try again. What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? ");
         } while (!validAnswer);
-/*        System.out.print("What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? ");
-        answer = input.nextInt();
-        while (randomNumbers[0] + randomNumbers[1] != answer) {
-            System.out.print("Wrong answer. Try again. What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? ");
-            answer = input.nextInt();
-        }*/
         System.out.println("You got it!");
     }
 }
 
 //                                                 _Output_
 /*
-What's 2 + 5? 7
+What's 8 + 9? 98
+Wrong answer. Try again. What's 8 + 9? 32
+Wrong answer. Try again. What's 8 + 9? 17
 You got it!
  */
