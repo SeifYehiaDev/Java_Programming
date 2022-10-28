504eec3c2f83ce435ee045152c67c7de408ae207
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-28 15:13:37 +0200

    calculate sum from 0 to 10 using while loop

---
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Examples/Example_05_02.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 28/10/2022
+ * */
+
+package Chapter_05_Examples;
+
+public class Example_05_02 {
+    public static void main(String[] args) {
+        int count = 0, sum = 0;
+        while (count < 10) {
+            sum += count;
+            count++;
+        }
+        System.out.println("Sum is " + sum);
+    }
+}
+
+//                                                 _Output_
+/*
+Sum is 45
+ */
