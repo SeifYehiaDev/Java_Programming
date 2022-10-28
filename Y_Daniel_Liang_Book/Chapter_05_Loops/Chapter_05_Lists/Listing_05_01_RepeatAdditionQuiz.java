8f2b24a081f2df4d8d63ad9ed6394a143431af0c
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-28 15:42:45 +0200

    Repeat addition quiz

--- Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Lists/Listing_05_01_RepeatAdditionQuiz.java
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Lists/Listing_05_01_RepeatAdditionQuiz.java
@@ -10,15 +10,15 @@ import java.util.Scanner;
 
 public class Listing_05_01_RepeatAdditionQuiz {
     public static void main(String[] args) {
-        int randomNumbers[] = new int[2], answer;
+        int randomNumbers[] = new int[3];
         boolean validAnswer = true;
         Scanner input = new Scanner(System.in);
         for (int index = 0; index < randomNumbers.length; index++)
             randomNumbers[index] = (int) (Math.random() * 10);
         do {
             System.out.print((validAnswer) ? "What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? " : "Wrong answer. Try again. What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? ");
-            answer = input.nextInt();
-            validAnswer = randomNumbers[0] + randomNumbers[1] == answer;
+            randomNumbers[2] = input.nextInt();
+            validAnswer = randomNumbers[0] + randomNumbers[1] == randomNumbers[2];
         } while (!validAnswer);
         System.out.println("You got it!");
     }
