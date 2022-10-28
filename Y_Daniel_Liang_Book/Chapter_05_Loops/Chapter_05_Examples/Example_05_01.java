5d01561bb44212148f787cc72b1a282f7fae2621
SeifYehia <SeifYehiaDev@outlook.com>
2022-10-28 14:52:59 +0200

    Display Welcome to Java! hundred times

---
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Examples/Example_05_01.java
@@ -0,0 +1,121 @@
+/*
+ * Author : Seif Yehia Esmail Abdel-Aal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 28/10/2022
+ * */
+
+package Chapter_05_Examples;
+
+public class Example_05_01 {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 100) {
+            System.out.println(count + 1 + "- " + "Welcome to Java!");
+            count++;
+        }
+    }
+}
+
+//                                                 _Output_
+/*
+1- Welcome to Java!
+2- Welcome to Java!
+3- Welcome to Java!
+4- Welcome to Java!
+5- Welcome to Java!
+6- Welcome to Java!
+7- Welcome to Java!
+8- Welcome to Java!
+9- Welcome to Java!
+10- Welcome to Java!
+11- Welcome to Java!
+12- Welcome to Java!
+13- Welcome to Java!
+14- Welcome to Java!
+15- Welcome to Java!
+16- Welcome to Java!
+17- Welcome to Java!
+18- Welcome to Java!
+19- Welcome to Java!
+20- Welcome to Java!
+21- Welcome to Java!
+22- Welcome to Java!
+23- Welcome to Java!
+24- Welcome to Java!
+25- Welcome to Java!
+26- Welcome to Java!
+27- Welcome to Java!
+28- Welcome to Java!
+29- Welcome to Java!
+30- Welcome to Java!
+31- Welcome to Java!
+32- Welcome to Java!
+33- Welcome to Java!
+34- Welcome to Java!
+35- Welcome to Java!
+36- Welcome to Java!
+37- Welcome to Java!
+38- Welcome to Java!
+39- Welcome to Java!
+40- Welcome to Java!
+41- Welcome to Java!
+42- Welcome to Java!
+43- Welcome to Java!
+44- Welcome to Java!
+45- Welcome to Java!
+46- Welcome to Java!
+47- Welcome to Java!
+48- Welcome to Java!
+49- Welcome to Java!
+50- Welcome to Java!
+51- Welcome to Java!
+52- Welcome to Java!
+53- Welcome to Java!
+54- Welcome to Java!
+55- Welcome to Java!
+56- Welcome to Java!
+57- Welcome to Java!
+58- Welcome to Java!
+59- Welcome to Java!
+60- Welcome to Java!
+61- Welcome to Java!
+62- Welcome to Java!
+63- Welcome to Java!
+64- Welcome to Java!
+65- Welcome to Java!
+66- Welcome to Java!
+67- Welcome to Java!
+68- Welcome to Java!
+69- Welcome to Java!
+70- Welcome to Java!
+71- Welcome to Java!
+72- Welcome to Java!
+73- Welcome to Java!
+74- Welcome to Java!
+75- Welcome to Java!
+76- Welcome to Java!
+77- Welcome to Java!
+78- Welcome to Java!
+79- Welcome to Java!
+80- Welcome to Java!
+81- Welcome to Java!
+82- Welcome to Java!
+83- Welcome to Java!
+84- Welcome to Java!
+85- Welcome to Java!
+86- Welcome to Java!
+87- Welcome to Java!
+88- Welcome to Java!
+89- Welcome to Java!
+90- Welcome to Java!
+91- Welcome to Java!
+92- Welcome to Java!
+93- Welcome to Java!
+94- Welcome to Java!
+95- Welcome to Java!
+96- Welcome to Java!
+97- Welcome to Java!
+98- Welcome to Java!
+99- Welcome to Java!
+100- Welcome to Java!
+ */
