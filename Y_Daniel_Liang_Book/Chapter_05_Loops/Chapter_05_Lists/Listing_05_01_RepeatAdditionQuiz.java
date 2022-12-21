7255bb616120c479668df17afc80b877e6b9bb1f
SeifYehia <SeifYehiaDev@outlook.com>
2022-12-21 16:23:53 +0200

    RepeatAdditionQuiz

--- Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Lists/Listing_05_01_RepeatAdditionQuiz.java
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Lists/Listing_05_01_RepeatAdditionQuiz.java
@@ -1,7 +1,7 @@
 /*
- * Author : Seif Yehia Esmail Abdel-Aal
+ * Author : Seif Yehia Esmail AbdelAal
  * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
- * Created on : 28/10/2022
+ * Created on : 21/12/2022
  * */
 
 package Chapter_05_Lists;
@@ -13,10 +13,10 @@ public class Listing_05_01_RepeatAdditionQuiz {
         int randomNumbers[] = new int[3];
         boolean validAnswer = true;
         Scanner input = new Scanner(System.in);
-        for (int index = 0; index < randomNumbers.length; index++)
+        for (int index = 0; index < 2; index++)
             randomNumbers[index] = (int) (Math.random() * 10);
         do {
-            System.out.print((validAnswer) ? "What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? " : "Wrong answer. Try again. What's " + randomNumbers[0] + " + " + randomNumbers[1] + "? ");
+            System.out.print(((validAnswer) ? "" : "Wrong answer. Try again. ").concat("What's is " + randomNumbers[0] + " + " + randomNumbers[1] + "? "));
             randomNumbers[2] = input.nextInt();
             validAnswer = randomNumbers[0] + randomNumbers[1] == randomNumbers[2];
         } while (!validAnswer);
