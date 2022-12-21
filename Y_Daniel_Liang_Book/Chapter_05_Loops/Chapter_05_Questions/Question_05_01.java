99e7b899d7b9687ca76904704aac1839459f0d19
SeifYehia <SeifYehiaDev@outlook.com>
2022-12-21 16:43:18 +0200

    Question_05_01

--- Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Questions/Question_05_01.java
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Questions/Question_05_01.java
@@ -1,7 +1,7 @@
 /*
- * Author : Seif Yehia Esmail Abdel-Aal
+ * Author : Seif Yehia Esmail AbdelAal
  * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
- * Created on : 28/10/2022
+ * Created on : 21/12/2022
  * */
 
 package Chapter_05_Questions;
@@ -14,12 +14,12 @@ public class Question_05_01 {
     public static void main(String[] args) {
         int count = 0;
         while (count < 10) {
-// Point A
+// Point A ====> Point A is always true
             System.out.println("Welcome to Java!");
             count++;
-// Point B
+// Point B ====> Point B is sometimes true or false
         }
-// Point C
+// Point C ====> Point C is always false
         System.out.println("**************************************************");
         System.out.println("Point A is always true");
         System.out.println("Point B is sometimes true or false");
