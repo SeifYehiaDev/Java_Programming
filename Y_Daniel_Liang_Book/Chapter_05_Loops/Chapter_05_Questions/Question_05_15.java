66d0683b45d259b00078c8bb2c79ad835274d0af
SeifYehia <SeifYehiaDev@outlook.com>
2022-12-22 18:41:14 +0200

    Question_05_15

---
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Questions/Question_05_15.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 22/12/2022
+ * */
+
+package Chapter_05_Questions;
+
+//                                                 _Question_
+            /*Can you convert a for loop to a while loop? List the advantages of using for loops.*/
+//                                                 _Solution_
+public class Question_05_15 {
+    public static void main(String[] args) {
+        System.out.println("Can you convert a for loop to a while loop?");
+        System.out.println("Yes");
+        System.out.println();
+        System.out.println("List the advantages of using for loops");
+        System.out.println("a for loop may be used if the number of repetitions is known in advance");
+    }
+}
+
+//                                                 _Output_
+/*
+Can you convert a for loop to a while loop?
+Yes
+
+List the advantages of using for loops
+a for loop may be used if the number of repetitions is known in advance
+ */
