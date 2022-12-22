4894734bbf2d15b52d0586e69d549aa8021eee11
SeifYehia <SeifYehiaDev@outlook.com>
2022-12-22 17:13:20 +0200

    Question_05_11

---
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Questions/Question_05_11.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 22/12/2022
+ * */
+
+package Chapter_05_Questions;
+
+//                                                 _Question_
+                                    /*What does the following statement do?*/
+//                                                 _Solution_
+public class Question_05_11 {
+    public static void main(String[] args) {
+  /*      for (; ; ) {
+            System.out.println("Infinite loop");
+        }*/
+
+/*        for (;true ; ) {
+            System.out.println("Infinite loop");
+        }*/
+/*        for (; ; System.out.println("Infinite loop")) ;*/
+        System.out.println("Infinite loop");
+    }
+}
+
+//                                                 _Output_
+/*
+Infinite loop
+ */
