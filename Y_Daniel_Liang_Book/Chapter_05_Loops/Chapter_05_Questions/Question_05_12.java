4a0b8683cc5aae652ae5681fe33cefd2dad386cf
SeifYehia <SeifYehiaDev@outlook.com>
2022-12-22 17:15:22 +0200

    Question_05_12

---
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Questions/Question_05_12.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 22/12/2022
+ * */
+
+package Chapter_05_Questions;
+
+//                                                 _Question_
+        /*If a variable is declared in a for loop control, can it be used after the loop exits?*/
+//                                                 _Solution_
+public class Question_05_12 {
+    public static void main(String[] args) {
+        System.out.println("If a variable is declared in a for loop control, can it be used after the loop exits?");
+        System.out.println("Nooooooooooooooooooooo ^_^");
+    }
+}
+
+//                                                 _Output_
+/*
+If a variable is declared in a for loop control, can it be used after the loop exits?
+Nooooooooooooooooooooo ^_^
+ */
