d2b42275d7ef263197eac09d9d5bc989a9f1a382
SeifYehia <SeifYehiaDev@outlook.com>
2022-12-22 16:45:27 +0200

    Example_05_06

---
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Examples/Example_05_06.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 22/12/2022
+ * */
+
+package Chapter_05_Examples;
+
+public class Example_05_06 {
+    public static void main(String[] args) {
+//      for (; ; System.out.println("Welcome to Java!")) ;
+        // Equivalent
+//      for (; true; System.out.println("Welcome to Java!")) ;
+        // Equivalent
+/*        while (true)
+            System.out.println("Welcome to Java!");*/
+    }
+}
+
+//                                                 _Output_
+/*
+1- infinite loop
+2- infinite loop
+3- infinite loop
+ */
