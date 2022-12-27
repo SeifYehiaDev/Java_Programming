556f79ed4fe56b604d6be13cb47168dbd128402f
SeifYehia <SeifYehiaDev@outlook.com>
2022-12-27 16:16:11 +0200

    Minimizing Numeric Errors

---
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Examples/Example_05_08.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/12/2022
+ * */
+
+package Chapter_05_Examples;
+
+public class Example_05_08 {
+    public static void main(String[] args) {
+        double sum = 0;
+        for (double i = 0.01; i <= 1.0; i = i + 0.01)
+            sum += i;
+        System.out.println("The sum is " + sum);
+    }
+}
+
+//                                                 _Output_
+/*
+
+ */
