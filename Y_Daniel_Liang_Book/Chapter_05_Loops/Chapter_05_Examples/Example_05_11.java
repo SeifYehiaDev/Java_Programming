b8b58a4dd4c0e147cce90b3d5599b9dfb7d15325
SeifYehia <SeifYehiaDev@outlook.com>
2022-12-29 21:48:31 +0200

    Write a program to find the smallest factor using break

---
+++ Y_Daniel_Liang_Book/Chapter_05_Loops/Chapter_05_Examples/Example_05_11.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (facebook , linkedin , instagram , github) : SeifYehiaDev
+ * Created on : 27/12/2022
+ * */
+
+package Chapter_05_Examples;
+
+import java.util.Scanner;
+
+public class Example_05_11 {
+    public static void main(String[] args) {
+        int factor = 2, number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        number = input.nextInt();
+        while (factor <= number) {
+            if (number % factor == 0) break;
+            factor++;
+        }
+        System.out.println("The smallest factor other than 1 for " + number + " is " + factor);
+    }
+}
+
+//                                                 _Output_
+/*
+Enter a number: 20
+The smallest factor other than 1 for 20 is 2
+ */
