6eccf82c8998266405f507e302fe925875e40adb
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-07 18:55:34 +0200

    Display five messages

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_02.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 07/01/2024
+ * */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_02 {
+    public static void main(String[] args) {
+        for (int index = 1; index <= 5; index++)
+            System.out.println("Welcome to Java.");
+    }
+}
+
+//                                                 _Output_
+/*
+Welcome to Java.
+Welcome to Java.
+Welcome to Java.
+Welcome to Java.
+Welcome to Java.
+ */
