a050d5d6c819aa90532c67489543d4041179227c
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-07 18:57:07 +0200

    Summation of a series

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_06.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 07/01/2024
+ * */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_06 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int index = 1; index <= 9; index++)
+            sum += index;
+        System.out.print("Summation of a series is " + sum);
+    }
+}
+
+//                                                 _Output_
+/*
+Summation of a series is 45
+ */
