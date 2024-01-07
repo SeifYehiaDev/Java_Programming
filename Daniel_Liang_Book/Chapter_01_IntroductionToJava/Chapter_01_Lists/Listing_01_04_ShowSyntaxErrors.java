d95f67fb7f4b207890c9d3028d6eb3932f4a110e
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-07 18:53:30 +0200

    ShowSyntaxErrors

---
+++ Daniel_Liang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_04_ShowSyntaxErrors.java
@@ -0,0 +1,18 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 07/01/2024
+ * */
+
+package Chapter_01_Lists;
+
+public class Listing_01_04_ShowSyntaxErrors {
+    public static void main(String[] args) {
+      //  System.out.println("Welcome to Java);
+    }
+}
+
+//                                                 _Output_
+/*
+java: unclosed string literal
+ */
