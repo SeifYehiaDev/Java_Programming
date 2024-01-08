86ba2b60441142dd8aa369dbb69a59c98c7799bb
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:30:00 +0200

    Question_02_05

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_05.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_05 {
+    public static void main(String[] args) {
+        /*Identify and fix the errors in the following code:*/
+        int k = 0; // declare and initialized before it can be used
+        int i = k + 2;
+        System.out.print(i);
+    }
+}
+
+//                                                 _Output_
+/*
+2
+ */
