6fb54fe8886bf41f23eff7d0313eaafaf7a1acad
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:31:23 +0200

    Question_02_14

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_14.java
@@ -0,0 +1,49 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_14 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ 2 * (5 / 2 + 5 / 2)
+                   2 * (2 + 2)
+                   2 * 4
+                   8
+                ■ 2 * 5 / 2 + 2 * 5 / 2
+                   10 / 2 + 2 * 5 / 2
+                   5 + 2 * 5 / 2
+                   5 + 10 / 2
+                   5 + 5
+                   10
+                ■ 2 * (5 / 2)
+                   2 * 2
+                   4
+                ■ 2 * 5 / 2
+                   10 / 2
+                   5""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ 2 * (5 / 2 + 5 / 2)
+   2 * (2 + 2)
+   2 * 4
+   8
+■ 2 * 5 / 2 + 2 * 5 / 2
+   10 / 2 + 2 * 5 / 2
+   5 + 2 * 5 / 2
+   5 + 10 / 2
+   5 + 5
+   10
+■ 2 * (5 / 2)
+   2 * 2
+   4
+■ 2 * 5 / 2
+   10 / 2
+   5
+ */
