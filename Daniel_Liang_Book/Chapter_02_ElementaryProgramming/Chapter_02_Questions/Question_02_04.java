f8659679f57447a78decc9bc93c7efb78011d469
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:29:53 +0200

    Question_02_04

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_04.java
@@ -0,0 +1,63 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_04 {
+    public static void main(String[] args) {
+        System.out.print("""
+                1- Which of the following identifiers are valid? Which are Java keywords?
+                miles, Test, a++, ––a, 4#R, $4, #44, apps
+                class, public, int, x, y, radius
+                ****************************** Answer ******************************
+                ■ Valid are miles, Test, $4, apps, x, y and radius
+                ■ Not Valid
+                            1- a++ ----> use a different digit ($) & (_)
+                            2- ––a ----> use a different digit ($) & (_)
+                            3- 4#R ----> start with number
+                            4- #44 ----> use a different digit ($) & (_)
+                            5- class ----> use a reserved word in java
+                            6- public ----> use a reserved word in java
+                            7- int ----> use a reserved word in java
+                                
+                2- Which are Java keywords?
+                    1- package
+                    2- public
+                    3- class
+                    4- void
+                    5- int
+                    6- static
+                    7- final
+                    8- new""");
+    }
+}
+
+//                                                 _Output_
+/*
+1- Which of the following identifiers are valid? Which are Java keywords?
+miles, Test, a++, ––a, 4#R, $4, #44, apps
+class, public, int, x, y, radius
+****************************** Answer ******************************
+■ Valid are miles, Test, $4, apps, x, y and radius
+■ Not Valid
+            1- a++ ----> use a different digit ($) & (_)
+            2- ––a ----> use a different digit ($) & (_)
+            3- 4#R ----> start with number
+            4- #44 ----> use a different digit ($) & (_)
+            5- class ----> use a reserved word in java
+            6- public ----> use a reserved word in java
+            7- int ----> use a reserved word in java
+
+2- Which are Java keywords?
+    1- package
+    2- public
+    3- class
+    4- void
+    5- int
+    6- static
+    7- final
+    8- new
+ */
