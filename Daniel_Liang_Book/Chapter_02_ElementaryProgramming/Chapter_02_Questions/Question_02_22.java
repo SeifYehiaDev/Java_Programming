af97d9bc22f67c41e847c8d2d8753a736c5a7bbf
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 13:33:25 +0200

    Question_02_22

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_22.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_22 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ How would you write the following arithmetic expression in Java?
+                  (a) 4.0 / (3 * (r + 34)) - 9.0 * (a + b * c) + (3 + d * (2 + a)) / (a + b * d)
+                  (b) 5.5 * Math.pow(r + 2.5, 2.5 + t)""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ How would you write the following arithmetic expression in Java?
+  (a) 4.0 / (3 * (r + 34)) - 9.0 * (a + b * c) + (3 + d * (2 + a)) / (a + b * d)
+  (b) 5.5 * Math.pow(r + 2.5, 2.5 + t)
+ */
