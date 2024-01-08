009c7f0c60de8a5df59106deada56259913e951a
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 13:32:52 +0200

    Question_02_19

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_19.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_19 {
+    public static void main(String[] args) {
+        System.out.print("""
+                Which of the following are correct literals for floating-point numbers?
+                12.3, 12.3e+2, 23.4e-2, –334.4, 20.5, 39F, 40D
+                    ■ All can be used as literals for floating-point numbers. In Java, floating-point numbers has two types: float and double.
+                      39F is a float type and others are of the double type.""");
+    }
+}
+
+//                                                 _Output_
+/*
+Which of the following are correct literals for floating-point numbers?
+12.3, 12.3e+2, 23.4e-2, –334.4, 20.5, 39F, 40D
+    ■ All can be used as literals for floating-point numbers. In Java, floating-point numbers has two types: float and double.
+      39F is a float type and others are of the double type.
+ */
