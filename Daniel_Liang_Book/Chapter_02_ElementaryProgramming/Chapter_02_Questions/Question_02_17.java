c85224b0351f7dd769ee0af722cb034272206e00
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:31:56 +0200

    Question_02_17

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_17.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_17 {
+    public static void main(String[] args) {
+        int m = 2, r = 3;
+        System.out.println("Suppose m and r are integers. Write a Java expression for mr^2 to obtain a floating-point result.");
+        System.out.println("m = " + m + ", r = " + r + " " + ", mr^2 = " + m * Math.pow(r, 2.0));
+    }
+}
+
+//                                                 _Output_
+/*
+Suppose m and r are integers. Write a Java expression for mr^2 to obtain a floating-point result.
+m = 2, r = 3 , mr^2 = 18.0
+ */
