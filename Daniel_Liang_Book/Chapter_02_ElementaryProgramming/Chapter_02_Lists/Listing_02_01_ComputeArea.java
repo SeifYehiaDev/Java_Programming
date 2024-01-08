42ae21742960e8ba809d14f51bc0755cae6e78fd
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:32:56 +0200

    ComputeArea

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Lists/Listing_02_01_ComputeArea.java
@@ -0,0 +1,20 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Lists;
+
+public class Listing_02_01_ComputeArea {
+    public static void main(String[] args) {
+        final double radius = 20.0D;
+        double area = Math.pow(radius, 2.0) * Math.PI;
+        System.out.print("The area for the circle of radius " + radius + " is " + area);
+    }
+}
+
+//                                                 _Output_
+/*
+The area for the circle of radius 20.0 is 1256.6370614359173
+ */
