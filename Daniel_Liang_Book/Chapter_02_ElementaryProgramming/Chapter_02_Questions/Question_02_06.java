bc76ab6218a3dc0448ef612b8f2a06bea49b8d0d
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:30:07 +0200

    Question_02_06

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_06.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_06 {
+    public static void main(String[] args) {
+        /*Identify and fix the errors in the following code:*/
+        int i, j, k;
+        i = j = k = 2;
+        System.out.print(i + " " + j + " " + k);
+    }
+}
+
+//                                                 _Output_
+/*
+2 2 2
+ */
