cc13783fa020d4bd49e34404399dcd976dd2f0d4
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:30:22 +0200

    Question_02_08

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_08.java
@@ -0,0 +1,37 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_08 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ What are the naming conventions for class names, method names, constants, and variables?
+                   (A) ClassName: Capitalize the first letter of each word in a class name.
+                   (B) MethodName & variablesName: Use lowercase If a name consists of several words, concatenate them into one, making the first word lowercase and capitalizing the first letter of each subsequent word.
+                   (C) Constants: Capitalize every letter in a constant, and use underscores between words.
+                   
+                ■ Which of the following items can be a constant, a method, a variable, or a class according to the Java naming conventions?
+                    1- MAX_VALUE: Constant
+                    2- Test: Class
+                    3- read: Variable
+                    4- readDouble: Method""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ What are the naming conventions for class names, method names, constants, and variables?
+   (A) ClassName: Capitalize the first letter of each word in a class name.
+   (B) MethodName & variablesName: Use lowercase If a name consists of several words, concatenate them into one, making the first word lowercase and capitalizing the first letter of each subsequent word.
+   (C) Constants: Capitalize every letter in a constant, and use underscores between words.
+
+■ Which of the following items can be a constant, a method, a variable, or a class according to the Java naming conventions?
+    1- MAX_VALUE: Constant
+    2- Test: Class
+    3- read: Variable
+    4- readDouble: Method
+ */
