33313751ba4f3a9f96752d7d131d42b794c27f7b
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:28:50 +0200

    Question_02_01

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_01.java
@@ -0,0 +1,22 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_01 {
+    public static void main(String[] args) {
+        /*Identify and fix the errors in the following code:*/
+        double i = 50.0;
+        double k = i + 50.0;
+        double j = k + 1;
+        System.out.println("j is " + j + " and k is " + k);
+    }
+}
+
+//                                                 _Output_
+/*
+j is 101.0 and k is 100.0
+ */
