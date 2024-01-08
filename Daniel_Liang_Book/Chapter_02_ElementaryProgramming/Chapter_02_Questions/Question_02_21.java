56f4e877fc799379a26fdef90558997a83a73348
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 13:33:13 +0200

    Question_02_21

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_21.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_21 {
+    public static void main(String[] args) {
+        System.out.print("""
+                Which of the following are correct literals?
+                5_2534e+1, _2534, 5_2, 5_
+                    ■ 5_2534e+1, and 5_2, are correct.""");
+    }
+}
+
+//                                                 _Output_
+/*
+Which of the following are correct literals?
+5_2534e+1, _2534, 5_2, 5_
+    ■ 5_2534e+1, and 5_2, are correct.
+ */
