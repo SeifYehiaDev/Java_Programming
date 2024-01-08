bb6f860348544bd364ee1616e6afd2a4d698d6e5
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:30:32 +0200

    Question_02_09

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_09.java
@@ -0,0 +1,22 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_09 {
+    public static void main(String[] args) {
+        final double KILOMETERS_PER_MILE = 1.6;
+        double miles = 100, kilometers = miles * KILOMETERS_PER_MILE;
+        System.out.print("What is kilometers after Step 4?\n");
+        System.out.print(kilometers);
+    }
+}
+
+//                                                 _Output_
+/*
+What is kilometers after Step 4?
+160.0
+ */
