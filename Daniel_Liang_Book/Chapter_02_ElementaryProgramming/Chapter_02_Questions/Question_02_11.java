13a4572c35d311b4f3bf9d5b7147374ea5b73d98
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:30:52 +0200

    Question_02_11

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_11.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_11 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Show the result of the following remainders.
+                   (A)  56 %  6 =  2
+                   (B)  78 % -4 =  2
+                   (C) -34 %  5 = -4
+                   (F) -34 % -5 = -4
+                   (D)   5 %  1 =  0""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Show the result of the following remainders.
+   (A)  56 %  6 =  2
+   (B)  78 % -4 =  2
+   (C) -34 %  5 = -4
+   (F) -34 % -5 = -4
+   (D)   5 %  1 =  0
+ */
