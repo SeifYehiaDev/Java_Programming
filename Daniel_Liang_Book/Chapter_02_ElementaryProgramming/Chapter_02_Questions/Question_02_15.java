0a6ec3fbb889369d0c3900f668baeaccdbaa49e3
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:31:36 +0200

    Question_02_15

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_15.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_15 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ 25 / 4 is 6
+                ■ 25 / 4.0 is 6.25
+                ■ 3 * 2 / 4 is 1
+                ■ 3.0 * 2 / 4 is 1.5""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ 25 / 4 is 6
+■ 25 / 4.0 is 6.25
+■ 3 * 2 / 4 is 1
+■ 3.0 * 2 / 4 is 1.5
+ */
