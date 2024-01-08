2f247bb264b08e13e6c7e386fea1f51d10af5cbb
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:30:14 +0200

    Question_02_07

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_07.java
@@ -0,0 +1,31 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_07 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ What are the benefits of using constants?
+                   (A) you don’t have to repeatedly type the same value if it is used multiple times.
+                   (B) if you have to change the constant value (e.g., from 3.14 to 3.14159 for PI), you need to change it only in a single location in the source code.
+                   (C) a descriptive name for a constant makes the program easy to read.
+                   
+                ■ Declare an int constant SIZE with value 20.
+                    final int SIZE = 20;""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ What are the benefits of using constants?
+   (A) you don’t have to repeatedly type the same value if it is used multiple times.
+   (B) if you have to change the constant value (e.g., from 3.14 to 3.14159 for PI), you need to change it only in a single location in the source code.
+   (C) a descriptive name for a constant makes the program easy to read.
+
+■ Declare an int constant SIZE with value 20.
+    final int SIZE = 20;
+ */
