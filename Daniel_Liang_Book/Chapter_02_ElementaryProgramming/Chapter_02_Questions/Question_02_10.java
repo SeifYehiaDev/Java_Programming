f817b3df13c610579b8d6885f9e8fd25ce77c804
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:30:42 +0200

    Question_02_10

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_10.java
@@ -0,0 +1,37 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_10 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Find the largest and smallest byte, short, int, long, float, and double.
+                   (A) byte:   1 byte  , 08 bits , min = -128   , max = +127
+                   (B) short:  2 bytes , 16 bits , min = -32768 , max = +32767
+                   (C) int:    4 bytes , 32 bits , min = -2_147_483_648 , max = +2_147_483_647
+                   (F) long:   8 bytes , 64 bits , min = -9_223_372_036_854_775_808 , max = +9_223_372_036_854_775_807
+                   (D) float:  4 bytes , 32 bits (32-bit IEEE 754)
+                   (E) double: 8 bytes , 64 bits (64-bit IEEE 754)
+                   
+                ■ Which of these data types requires the least amount of memory?
+                    * byte""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Find the largest and smallest byte, short, int, long, float, and double.
+   (A) byte:   1 byte  , 08 bits , min = -128   , max = +127
+   (B) short:  2 bytes , 16 bits , min = -32768 , max = +32767
+   (C) int:    4 bytes , 32 bits , min = -2_147_483_648 , max = +2_147_483_647
+   (F) long:   8 bytes , 64 bits , min = -9_223_372_036_854_775_808 , max = +9_223_372_036_854_775_807
+   (D) float:  4 bytes , 32 bits (32-bit IEEE 754)
+   (E) double: 8 bytes , 64 bits (64-bit IEEE 754)
+
+■ Which of these data types requires the least amount of memory?
+    * byte
+ */
