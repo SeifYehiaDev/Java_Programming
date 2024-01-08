da4182b00d5d07aca73c840fa7b1ddef7e2025a1
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 13:33:04 +0200

    Question_02_20

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_20.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_20 {
+    public static void main(String[] args) {
+        System.out.print("""
+                Which of the following are the same as 52.534?
+                5.2534e+1, 0.52534e+2, 525.34e-1, 5.2534e+0
+                    ■ 5.2534e+1, 0.52534e+2, 525.34e-1 are the same as 52.534.""");
+    }
+}
+
+//                                                 _Output_
+/*
+Which of the following are the same as 52.534?
+5.2534e+1, 0.52534e+2, 525.34e-1, 5.2534e+0
+    ■ 5.2534e+1, 0.52534e+2, 525.34e-1 are the same as 52.534.
+ */
