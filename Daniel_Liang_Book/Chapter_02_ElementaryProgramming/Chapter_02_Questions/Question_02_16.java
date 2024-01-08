cb741d6a8cf11db397b1eb4f4a0f3447ff257904
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:31:47 +0200

    Question_02_16

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_16.java
@@ -0,0 +1,24 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_16 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Write a statement to display the result of 2^3.5
+                   System.out.println(Math.pow(2, 3.5));
+                   """);
+        System.out.println(Math.pow(2, 3.5));
+    }
+}
+
+//                                                 _Output_
+/*
+■ Write a statement to display the result of 2^3.5
+   System.out.println(Math.pow(2, 3.5));
+11.313708498984761
+ */
