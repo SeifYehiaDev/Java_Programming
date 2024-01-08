30603814c568a991866f8159cd81a3848f64210f
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 10:31:11 +0200

    Question_02_13

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_13.java
@@ -0,0 +1,27 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_13 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ What is the result of 25 / 4?
+                   25 / 4 = 6
+                ■ How would you rewrite the expression if you wished the result to be a floating-point number?
+                   25.0 / 4 = 6.25
+                   25 / 4.0 = 6.25""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ What is the result of 25 / 4?
+   25 / 4 = 6
+■ How would you rewrite the expression if you wished the result to be a floating-point number?
+   25.0 / 4 = 6.25
+   25 / 4.0 = 6.25
+ */
