2a202d7da1861592a463564c9e1e6a1c2ee0d313
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-08 13:32:38 +0200

    Question_02_18

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_18.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 08/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_18 {
+    public static void main(String[] args) {
+        System.out.print("""
+                How many accurate digits are stored in a float or double type variable?
+                 ■ float:  A float value has 7 to 8 number of significant digits.
+                 ■ double: A double value has 15 to 17 number of significant digits.""");
+    }
+}
+
+//                                                 _Output_
+/*
+How many accurate digits are stored in a float or double type variable?
+ ■ float:  A float value has 7 to 8 number of significant digits.
+ ■ double: A double value has 15 to 17 number of significant digits.
+ */
