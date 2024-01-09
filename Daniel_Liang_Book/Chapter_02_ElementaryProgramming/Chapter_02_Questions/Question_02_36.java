e44f9922650f6b9eeae7ca3f4b50f869519e65cf
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:25:18 +0200

    Question_02_36

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_36.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_36 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Will overflow cause a runtime error?
+                No""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Will overflow cause a runtime error?
+No
+ */
