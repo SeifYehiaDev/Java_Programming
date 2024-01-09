f6e7ad1e76eeec99077542a0ce3eb6eedbcd28dd
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:24:38 +0200

    Question_02_32

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_32.java
@@ -0,0 +1,22 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_32 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ How would you write the following arithmetic expression?
+                z = (-b + Math.sqrt(Math.pow(b, 2.0) - 4 * a * c)) / 2 * a;
+                """);
+    }
+}
+
+//                                                 _Output_
+/*
+■ How would you write the following arithmetic expression?
+z = (-b + Math.sqrt(Math.pow(b, 2.0) - 4 * a * c)) / 2 * a;
+ */
