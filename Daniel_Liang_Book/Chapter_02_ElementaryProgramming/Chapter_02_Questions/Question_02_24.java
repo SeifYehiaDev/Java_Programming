cce69bc40dcd0739acbf19006c37b58d973e27ca
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:23:06 +0200

    Question_02_24

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_24.java
@@ -0,0 +1,26 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_24 {
+    public static void main(String[] args) {
+        System.out.println("Show the output of the following code: ");
+        double a = 6.5;
+        a += a + 1;
+        System.out.println(a);
+        a = 6;
+        a /= 2;
+        System.out.print(a);
+    }
+}
+
+//                                                 _Output_
+/*
+Show the output of the following code:
+14.0
+3.0
+ */
