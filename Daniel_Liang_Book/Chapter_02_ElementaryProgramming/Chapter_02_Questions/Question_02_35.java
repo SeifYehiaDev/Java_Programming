03d25c0d7f70db9783e4fe8aea78cb16beb4a10e
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:25:08 +0200

    Question_02_35

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_35.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_35 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ What is an integer overflow?
+                Numbers are stored with a limited numbers of digits. When a variable is assigned a value that is too large (in size) to be stored, it causes overflow.
+                ■ Can floating-point operations cause overflow?
+                Overflow is for integer operations. Floating-point operations will not cause overflow.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ What is an integer overflow?
+Numbers are stored with a limited numbers of digits. When a variable is assigned a value that is too large (in size) to be stored, it causes overflow.
+■ Can floating-point operations cause overflow?
+Overflow is for integer operations. Floating-point operations will not cause overflow.
+ */
