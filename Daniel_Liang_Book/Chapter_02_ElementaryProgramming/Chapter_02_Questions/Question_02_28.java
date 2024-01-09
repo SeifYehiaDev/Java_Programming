bff0f7e644fcd55e88975d39000576b192eaa43f
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:23:53 +0200

    Question_02_28

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_28.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_28 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ What does an explicit casting from a double to an int do with the fractional part of the double value?
+                 The fractional part is truncated.
+                ■ Does casting change the variable being cast?
+                 Casting does not change the variable being cast.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ What does an explicit casting from a double to an int do with the fractional part of the double value?
+ The fractional part is truncated.
+■ Does casting change the variable being cast?
+ Casting does not change the variable being cast.
+ */
