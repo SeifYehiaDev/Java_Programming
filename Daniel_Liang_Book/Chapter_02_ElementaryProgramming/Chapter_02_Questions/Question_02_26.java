06bd248a9e761675058ae4489d1d7bd3294dc3fe
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:23:31 +0200

    Question_02_26

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_26.java
@@ -0,0 +1,30 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_26 {
+    public static void main(String[] args) {
+        System.out.println("Show the output of the following code:");
+        int a = 6;
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+        a = 6;
+        b = ++a;
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
+
+//                                                 _Output_
+/*
+Show the output of the following code:
+7
+6
+7
+7
+ */
