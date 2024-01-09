953b69c975c92b426bbf9c8d03d87d535988f45d
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:24:26 +0200

    Question_02_31

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_31.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_31 {
+    public static void main(String[] args) {
+        System.out.println("Show the output of the following code: ");
+        double amount = 5;
+        System.out.println(amount / 2);
+        System.out.print(5 / 2);
+    }
+}
+
+//                                                 _Output_
+/*
+Show the output of the following code:
+2.5
+2
+ */
