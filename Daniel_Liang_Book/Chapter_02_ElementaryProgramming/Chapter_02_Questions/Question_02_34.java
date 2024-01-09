b641fbabb8dc63e382dc7fac96603f74700e116b
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:24:58 +0200

    Question_02_34

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_34.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_34 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Can you declare a variable as int and later redeclare it as double?
+                No""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Can you declare a variable as int and later redeclare it as double?
+No
+ */
