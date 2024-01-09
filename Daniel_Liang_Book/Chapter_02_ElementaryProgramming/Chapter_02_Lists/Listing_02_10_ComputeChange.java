538be29431cd7de9527d38740aeee679b91e027a
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:26:26 +0200

    ComputeChange

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Lists/Listing_02_10_ComputeChange.java
@@ -0,0 +1,46 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Lists;
+
+import java.util.Scanner;
+
+public class Listing_02_10_ComputeChange {
+    public static void main(String[] args) {
+        int remainingAmount, numberOfOneDollars, numberOfQuarters, numberOfDimes, numberOfNickels, numberOfPennies;
+        double amount;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an amount in double, for example 11.56: ");
+        amount = input.nextDouble();
+        remainingAmount = (int) (amount * 100);
+        numberOfOneDollars = remainingAmount / 100;
+        numberOfQuarters = (remainingAmount %= 100) / 25;
+        numberOfDimes = (remainingAmount %= 25) / 10;
+        numberOfNickels = (remainingAmount %= 10) / 5;
+        numberOfPennies = (remainingAmount %= 5);
+        System.out.printf("""
+                        %-2d %s
+                        %-2d %s
+                        %-2d %s
+                        %-2d %s
+                        %-2d %s""",
+                numberOfOneDollars, (numberOfOneDollars > 1) ? "dollars" : "dollar",
+                numberOfQuarters, (numberOfQuarters > 1) ? "quarters" : "quarter",
+                numberOfDimes, (numberOfDimes > 1) ? "dimes" : "dime",
+                numberOfNickels, (numberOfNickels > 1) ? "nickels" : "nickel",
+                numberOfPennies, (numberOfPennies > 1) ? "pennies" : "pennie");
+    }
+}
+
+//                                                 _Output_
+/*
+Enter an amount in double, for example 11.56: 11.56
+11 dollars
+2  quarters
+0  dime
+1  nickel
+1  pennie
+ */
