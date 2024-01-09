41fced4a802575b72902b2721383ff8d30aa91b8
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:23:43 +0200

    Question_02_27

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_27.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_27 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Can different types of numeric values be used together in a computation?
+                Yes. Different types of numeric values can be used in the same computation through numeric conversions referred to as casting.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Can different types of numeric values be used together in a computation?
+Yes. Different types of numeric values can be used in the same computation through numeric conversions referred to as casting.
+ */
