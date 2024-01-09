5b9802236591ffa8f53dc536a1020e1a802f5029
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:23:21 +0200

    Question_02_25

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_25.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_25 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Which of these statements are true?
+                    a. Any expression can be used as a statement.
+                    b. The expression x++ can be used as a statement.
+                    c. The statement x = x + 5 is also an expression.
+                    d. The statement x = y = x = 0 is illegal.
+                b and c are true.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Which of these statements are true?
+    a. Any expression can be used as a statement.
+    b. The expression x++ can be used as a statement.
+    c. The statement x = x + 5 is also an expression.
+    d. The statement x = y = x = 0 is illegal.
+b and c are true.
+ */
