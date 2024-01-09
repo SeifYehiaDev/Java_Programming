a2fdd1798cc70f7206aecca0d8241b6c8c9679ce
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-09 22:25:29 +0200

    Question_02_37

---
+++ Daniel_Liang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Questions/Question_02_37.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 09/01/2024
+ * */
+
+package Chapter_02_Questions;
+
+public class Question_02_37 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ What is a round-off error?
+                A round-off error, also called a rounding error, is the difference between the calculated approximation of a number and its exact mathematical value.
+                ■ Can integer operations cause round-off errors?
+                Integer operations will not cause rounding error.
+                ■ Can floating-point operations cause round-off errors?
+                Floating-point operations may cause rounding error.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ What is a round-off error?
+A round-off error, also called a rounding error, is the difference between the calculated approximation of a number and its exact mathematical value.
+■ Can integer operations cause round-off errors?
+Integer operations will not cause rounding error.
+■ Can floating-point operations cause round-off errors?
+Floating-point operations may cause rounding error.
+ */
