a44bf6a529d340f7b62fc87f9d7dcb22e20d0711
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:42:23 +0200

    Question_03_20

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_20.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_20 {
+    public static void main(String[] args) {
+        System.out.print("""
+                (a) Write a Boolean expression for |x - 5| < 4.5.
+                Math.abs(x - 5.0) < 4.5
+                                
+                (b) Write a Boolean expression for |x - 5| > 4.5.
+                Math.abs(x - 5.0) > 4.5""");
+    }
+}
+
+//                                                 _Output_
+/*
+
+ */
