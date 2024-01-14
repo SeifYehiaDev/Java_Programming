80f075ea5dd0b20d5666bea579b010f6e46defe6
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:45:00 +0200

    Question_03_37

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_37.java
@@ -0,0 +1,31 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_37 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ true || true && false
+                true || false
+                true
+                
+                ■ true && true || false
+                true || false
+                true""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ true || true && false
+true || false
+true
+
+■ true && true || false
+true || false
+true
+ */
