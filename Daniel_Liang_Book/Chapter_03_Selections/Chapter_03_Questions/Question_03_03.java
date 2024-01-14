395b3d1cfa2cc2d5f221771da9f780764979c6cb
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:39:43 +0200

    Question_03_03

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_03.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 11/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_03 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Can the following conversions involving casting be allowed?
+                    boolean b = true;
+                    i = (int)b;
+                No. Boolean values cannot be cast to other types.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Can the following conversions involving casting be allowed?
+    boolean b = true;
+    i = (int)b;
+No. Boolean values cannot be cast to other types.
+ */
