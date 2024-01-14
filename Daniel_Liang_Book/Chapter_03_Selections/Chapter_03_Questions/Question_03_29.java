7d6dadfd6e19d0aadc23743842d8904deff933bb
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:43:35 +0200

    Question_03_29

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_29.java
@@ -0,0 +1,39 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_29 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ What data types are required for a switch variable?
+                Switch variables must be of char, byte, short, int, or String types.
+                
+                ■ If the keyword break is not used after a case is processed, what is the next statement to be executed?
+                If a break statement is not used, the next case statement is performed.
+                
+                ■ Can you convert a switch statement to an equivalent if statement, or vice versa?
+                You can always convert a switch statement to an equivalent if statement, but not an if statement to a switch statement.
+                
+                ■ What are the advantages of using a switch statement?
+                The use of the switch statement can improve readability of the program in some cases. The compiled code for the switch statement is also more efficient than its corresponding if statement.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ What data types are required for a switch variable?
+Switch variables must be of char, byte, short, int, or String types.
+
+■ If the keyword break is not used after a case is processed, what is the next statement to be executed?
+If a break statement is not used, the next case statement is performed.
+
+■ Can you convert a switch statement to an equivalent if statement, or vice versa?
+You can always convert a switch statement to an equivalent if statement, but not an if statement to a switch statement.
+
+■ What are the advantages of using a switch statement?
+The use of the switch statement can improve readability of the program in some cases. The compiled code for the switch statement is also more efficient than its corresponding if statement.
+ */
