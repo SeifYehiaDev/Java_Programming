3de25aaf6e959542ed09f4a0b4c93296c09f53bf
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:41:17 +0200

    Question_03_12

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_12.java
@@ -0,0 +1,31 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_12 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Rewrite the following statement using a Boolean expression:
+                        if (count % 10 == 0)
+                        newLine = true;
+                        else
+                        newLine = false;
+                        
+                boolean newLine = (count % 10 == 0);""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Rewrite the following statement using a Boolean expression:
+        if (count % 10 == 0)
+        newLine = true;
+        else
+        newLine = false;
+
+boolean newLine = (count % 10 == 0);
+ */
