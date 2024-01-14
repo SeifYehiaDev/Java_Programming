42f59eb83742060d5b72d5cec7ff26af6c7a3dde
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:38:58 +0200

    Question_03_01

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_01.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 11/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_01 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ List six relational operators.
+                <, <=, ==, !=, >, >=""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ List six relational operators.
+<, <=, ==, !=, >, >=
+ */
