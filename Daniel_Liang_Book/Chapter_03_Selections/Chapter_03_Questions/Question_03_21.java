1f1e188a9d35d7893a3a61021809176561e969dd
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:42:31 +0200

    Question_03_21

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_21.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_21 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ x > y > 0 is incorrect
+                ■ x = y && y is incorrect
+                ■ x /= y is correct
+                ■ x or y is incorrect
+                ■ x and y is incorrect
+                ■ (x != 0) || (x = 0) is incorrect on x = 0. It should be x == 0.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ x > y > 0 is incorrect
+■ x = y && y is incorrect
+■ x /= y is correct
+■ x or y is incorrect
+■ x and y is incorrect
+■ (x != 0) || (x = 0) is incorrect on x = 0. It should be x == 0.
+ */
