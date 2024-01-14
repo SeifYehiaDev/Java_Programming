9e65b1fac678214d969605bc94b2fffb9ff5c03e
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:41:08 +0200

    Question_03_11

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_11.java
@@ -0,0 +1,26 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_11 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Which of the following statements are equivalent?
+                a, c, and d are the same.
+                ■ Which ones are correctly indented?
+                (B) and (C) are correctly indented
+                """);
+    }
+}
+
+//                                                 _Output_
+/*
+■ Which of the following statements are equivalent?
+a, c, and d are the same.
+■ Which ones are correctly indented?
+(B) and (C) are correctly indented
+ */
