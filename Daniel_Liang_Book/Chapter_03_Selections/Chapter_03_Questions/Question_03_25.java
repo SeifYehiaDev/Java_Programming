554930e44a607c324943fb6a6d922cfaceab96d1
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:43:05 +0200

    Question_03_25

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_25.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_25 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Write a Boolean expression that evaluates to true if age is greater than 13 and less than 18.
+                age > 13 && age < 18""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Write a Boolean expression that evaluates to true if age is greater than 13 and less than 18.
+age > 13 && age < 18
+ */
