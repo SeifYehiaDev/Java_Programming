3fc116442ab60d54ca9c24e3ce152c03ba19ebcb
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:39:15 +0200

    Question_03_02

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_02.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 11/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_02 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Assuming that x is 1, show the result of the following Boolean expressions:
+                 1- (x > 0)  = true
+                 2- (x < 0)  = false
+                 3- (x != 0) = true
+                 4- (x >= 0) = true
+                 5- (x != 1) = false""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Assuming that x is 1, show the result of the following Boolean expressions:
+ 1- (x > 0)  = true
+ 2- (x < 0)  = false
+ 3- (x != 0) = true
+ 4- (x >= 0) = true
+ 5- (x != 1) = false
+ */
