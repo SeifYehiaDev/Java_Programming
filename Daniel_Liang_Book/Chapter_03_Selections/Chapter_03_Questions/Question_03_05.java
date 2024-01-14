d7c4702323da59f4759f05397011fe816f22b951
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:39:59 +0200

    Question_03_05

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_05.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 11/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_05 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Write an if statement that increases pay by 3% if score is greater than 90.
+                   if (score > 90)
+                     pay *= 1.03;""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Write an if statement that increases pay by 3% if score is greater than 90.
+   if (score > 90)
+     pay *= 1.03;
+ */
