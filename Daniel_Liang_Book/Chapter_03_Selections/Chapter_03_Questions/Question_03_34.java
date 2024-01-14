f58ba49f8fb1da90cd4d402353c967ea77ac0b6b
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:44:35 +0200

    Question_03_34

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_34.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_34 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Rewrite the following if statements using the conditional operator.
+                ticketPrice = (ages >= 16) ? 20 : 10;""");
+    }
+}
+
+//                                                 _Output_
+/*
+Rewrite the following if statements using the conditional operator.
+ticketPrice = (ages >= 16) ? 20 : 10;
+ */
