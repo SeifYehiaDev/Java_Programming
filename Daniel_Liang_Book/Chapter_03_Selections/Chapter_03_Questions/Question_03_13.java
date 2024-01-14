d392dc8425725a6e88760453a84f0fe1191cd58d
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:41:25 +0200

    Question_03_13

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_13.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_13 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Are the following statements correct? Which one is better?
+                Both are correct. (b) is better.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Are the following statements correct? Which one is better?
+Both are correct. (b) is better.
+ */
