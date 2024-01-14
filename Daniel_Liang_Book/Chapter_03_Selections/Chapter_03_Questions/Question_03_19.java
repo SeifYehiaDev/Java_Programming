c491e3ecebbee316dfc9f3d393cbd18eaabfffc3
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:42:14 +0200

    Question_03_19

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_19.java
@@ -0,0 +1,27 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_19 {
+    public static void main(String[] args) {
+        System.out.print("""
+                (a) Write a Boolean expression that evaluates to true if a number stored in variable num is between 1 and 100.
+                (number > 1 && number < 100)
+                                
+                (b) Write a Boolean expression that evaluates to true if a number stored in variable num is between 1 and 100 or the number is negative.
+                (number > 1 && number < 100) || (number < 0)""");
+    }
+}
+
+//                                                 _Output_
+/*
+(a) Write a Boolean expression that evaluates to true if a number stored in variable num is between 1 and 100.
+(number > 1 && number < 100)
+
+(b) Write a Boolean expression that evaluates to true if a number stored in variable num is between 1 and 100 or the number is negative.
+(number > 1 && number < 100) || (number < 0)
+ */
