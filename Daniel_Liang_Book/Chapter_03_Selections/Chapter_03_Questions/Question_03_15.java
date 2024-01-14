2139bec9c6a63c97ffe80ca03a65196b87a3c910
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:41:42 +0200

    Question_03_15

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_15.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_15 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Which of the following is a possible output from invoking Math.random()?
+                0.5, 0.0 and 0.234""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Which of the following is a possible output from invoking Math.random()?
+0.5, 0.0 and 0.234
+ */
