c713a973a71417a85b9706b86b01c4d21e54b01b
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:45:27 +0200

    Question_03_40

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_40.java
@@ -0,0 +1,33 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_40 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Is (x > 0 && x < 10) the same as ((x > 0) && (x < 10))?
+                Yes
+                
+                ■ Is (x > 0 || x < 10) the same as ((x > 0) || (x < 10))?
+                Yes
+                
+                ■ Is (x > 0 || x < 10 && y < 0) the same as (x > 0 || (x < 10 && y < 0))?
+                Yes""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Is (x > 0 && x < 10) the same as ((x > 0) && (x < 10))?
+Yes
+
+■ Is (x > 0 || x < 10) the same as ((x > 0) || (x < 10))?
+Yes
+
+■ Is (x > 0 || x < 10 && y < 0) the same as (x > 0 || (x < 10 && y < 0))?
+Yes
+ */
