df0e97fdb31e471172ddd561e5ad19059c231fe5
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:39:51 +0200

    Question_03_04

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_04.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 11/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_04 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Write an if statement that assigns 1 to x if y is greater than 0.
+                   if (y > 0)
+                     x = 1;""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Write an if statement that assigns 1 to x if y is greater than 0.
+   if (y > 0)
+     x = 1;
+ */
