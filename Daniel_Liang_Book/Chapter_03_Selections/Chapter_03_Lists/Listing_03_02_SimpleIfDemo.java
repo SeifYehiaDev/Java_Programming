0884a2f0f34946b0e215a7d5b71637cc91df01e0
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:45:54 +0200

    SimpleIfDemo

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Lists/Listing_03_02_SimpleIfDemo.java
@@ -0,0 +1,33 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 11/01/2024
+ * */
+
+package Chapter_03_Lists;
+
+import java.util.Scanner;
+
+public class Listing_03_02_SimpleIfDemo {
+    public static void main(String[] args) {
+        int number;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        number = input.nextInt();
+        if (number % 5 == 0) System.out.println("HiFive");
+        if (number % 2 == 0) System.out.print("HiEven");
+    }
+}
+
+//                                                 _Output One_
+/*
+Enter an integer: 4
+HiEven
+ */
+
+//                                                 _Output Two_
+/*
+Enter an integer: 30
+HiFive
+HiEven
+ */
