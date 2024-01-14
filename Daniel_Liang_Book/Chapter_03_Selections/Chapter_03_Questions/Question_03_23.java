8ab244f5ad09a1c7e2d665e7726f7b7e386353a6
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:42:48 +0200

    Question_03_23

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_23.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_23 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ What is the value of the expression x >= 50 && x <= 100 if x is 45, 67, or 101?
+                If x is 45, the expression is false.
+                If x is 67, the expression is true.
+                If x is 101, the expression is false.""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ What is the value of the expression x >= 50 && x <= 100 if x is 45, 67, or 101?
+If x is 45, the expression is false.
+If x is 67, the expression is true.
+If x is 101, the expression is false.
+ */
