9ffdc072fa2bbc32c71990659b3760ae23f3c304
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:42:40 +0200

    Question_03_22

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_22.java
@@ -0,0 +1,25 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_22 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Are the following two expressions the same?
+                (a) x % 2 == 0 && x % 3 == 0
+                (b) x % 6 == 0
+                Yes""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Are the following two expressions the same?
+(a) x % 2 == 0 && x % 3 == 0
+(b) x % 6 == 0
+Yes
+ */
