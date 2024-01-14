b8feb76e234c0e331a273ec02b9a2d672a18f8ba
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:43:14 +0200

    Question_03_26

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_26.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_26 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Write a Boolean expression that evaluates to true if weight is greater than 50 pounds or height is greater than 60 inches.
+                weight > 50 || height > 60""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Write a Boolean expression that evaluates to true if weight is greater than 50 pounds or height is greater than 60 inches.
+weight > 50 || height > 60
+ */
