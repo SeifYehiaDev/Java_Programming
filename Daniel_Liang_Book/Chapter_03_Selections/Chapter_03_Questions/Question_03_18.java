b6b13b95dee4f54cf4de79db2d41972a73f2038f
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:42:06 +0200

    Question_03_18

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_18.java
@@ -0,0 +1,29 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_18 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ (true) && (3 > 4)     ====> false
+                ■ !(x > 0) && (x > 0)   ====> false
+                ■ (x > 0) || (x < 0)    ====> true
+                ■ (x != 0) || (x == 0)  ====> true
+                ■ (x >= 0) || (x < 0)   ====> true
+                ■ (x != 1) == !(x == 1) ====> true""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ (true) && (3 > 4)     ====> false
+■ !(x > 0) && (x > 0)   ====> false
+■ (x > 0) || (x < 0)    ====> true
+■ (x != 0) || (x == 0)  ====> true
+■ (x >= 0) || (x < 0)   ====> true
+■ (x != 1) == !(x == 1) ====> true
+ */
