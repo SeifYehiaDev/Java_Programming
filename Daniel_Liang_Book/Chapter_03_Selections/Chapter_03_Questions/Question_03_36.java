bc73ccc9f9bf5f64332becc43995da91c2f7dfcb
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:44:49 +0200

    Question_03_36

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_36.java
@@ -0,0 +1,21 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_36 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Write conditional expression that returns -1 or 1 randomly.
+                (int)(Math.random() * 2) == 0 ? -1 : 1;""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Write conditional expression that returns -1 or 1 randomly.
+(int)(Math.random() * 2) == 0 ? -1 : 1;
+ */
