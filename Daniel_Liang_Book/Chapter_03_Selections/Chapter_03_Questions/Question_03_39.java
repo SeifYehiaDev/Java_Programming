f0b78f4f0c5243f5ea31aa55882a7215b32fc1d3
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-14 19:45:19 +0200

    Question_03_39

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Questions/Question_03_39.java
@@ -0,0 +1,45 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 14/01/2024
+ * */
+
+package Chapter_03_Questions;
+
+public class Question_03_39 {
+    public static void main(String[] args) {
+        System.out.print("""
+                ■ Evaluate the following expressions:
+                a- 2 * 2 - 3 > 2 && 4 – 2 > 5
+                    4 - 3 > 2 && 4 - 2 > 5
+                    1 > 2 && 4 - 2 > 5
+                    1 > 2 && 2 > 5
+                    false && false 
+                    false
+                    
+                b- 2 * 2 - 3 > 2 && 4 – 2 > 5
+                    4 - 3 > 2 || 4 - 2 > 5
+                    1 > 2 || 4 - 2 > 5
+                    1 > 2 || 2 > 5
+                    false || false 
+                    false""");
+    }
+}
+
+//                                                 _Output_
+/*
+■ Evaluate the following expressions:
+a- 2 * 2 - 3 > 2 && 4 – 2 > 5
+    4 - 3 > 2 && 4 - 2 > 5
+    1 > 2 && 4 - 2 > 5
+    1 > 2 && 2 > 5
+    false && false
+    false
+
+b- 2 * 2 - 3 > 2 && 4 – 2 > 5
+    4 - 3 > 2 || 4 - 2 > 5
+    1 > 2 || 4 - 2 > 5
+    1 > 2 || 2 > 5
+    false || false
+    false
+ */
