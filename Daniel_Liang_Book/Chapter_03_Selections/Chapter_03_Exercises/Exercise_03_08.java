b630651167cac258d395ff35490bbf53fc7945b5
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-01-15 18:40:59 +0200

    Sort three integers

---
+++ Daniel_Liang_Book/Chapter_03_Selections/Chapter_03_Exercises/Exercise_03_08.java
@@ -0,0 +1,30 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 15/01/2024
+ * */
+
+package Chapter_03_Exercises;
+
+import java.util.Scanner;
+
+public class Exercise_03_08 {
+    public static void main(String[] args) {
+        int firstNumber, secondNumber, thirdNumber;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter three integers: ");
+        firstNumber = input.nextInt();
+        secondNumber = input.nextInt();
+        thirdNumber = input.nextInt();
+        if (firstNumber > secondNumber) firstNumber = (firstNumber + secondNumber) - (secondNumber = firstNumber);
+        if (secondNumber > thirdNumber) secondNumber = (secondNumber + thirdNumber) - (thirdNumber = secondNumber);
+        if (firstNumber > secondNumber) firstNumber = (firstNumber + secondNumber) - (secondNumber = firstNumber);
+        System.out.printf("After sorting in non-decreasing order: %d %d %d", firstNumber, secondNumber, thirdNumber);
+    }
+}
+
+//                                                 _Output_
+/*
+Enter three integers: 100 90 80
+After sorting in non-decreasing order: 80 90 100
+ */
