8782930937cbe55b055beba1eb99b323b9062dd2
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-07-04 02:13:27 +0300

    WelcomeWithThreeMessages

---
+++ DanielLiang_Book/Chapter_01_IntroductionToJava/Chapter_01_Lists/Listing_01_02_WelcomeWithThreeMessages.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 04/07/2024
+ * */
+
+package Chapter_01_Lists;
+
+public class Listing_01_02_WelcomeWithThreeMessages {
+    public static void main(String[] args) {
+        System.out.print("""
+                Programming is fun!
+                Fundamentals First
+                Problem Driven""");
+    }
+}
+
+//                                                 _Output_
+/*
+Programming is fun!
+Fundamentals First
+Problem Driven
+ */
