7838cae430c937f710501c1205a2c34e23d9b2e3
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-07-04 01:53:26 +0300

    Display three messages

---
+++ DanielLiang_Book/Chapter_01_IntroductionToJava/Chapter_01_Exercises/Exercise_01_01.java
@@ -0,0 +1,23 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 04/07/2024
+ * */
+
+package Chapter_01_Exercises;
+
+public class Exercise_01_01 {
+    public static void main(String[] args) {
+        System.out.print("""
+                Welcome to Java.
+                Welcome to Computer Science.
+                Programming is fun.""");
+    }
+}
+
+//                                                 _Output_
+/*
+Welcome to Java.
+Welcome to Computer Science.
+Programming is fun.
+ */
