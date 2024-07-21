820f46c6ee173acce2a5c57c874d274242e10cc7
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-07-22 00:59:38 +0300

    ShowCurrentTime

---
+++ DanielLiang_Book/Chapter_02_ElementaryProgramming/Chapter_02_Lists/Listing_02_07_ShowCurrentTime.java
@@ -0,0 +1,18 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 22/07/2024
+ * */
+
+package Chapter_02_Lists;
+
+public class Listing_02_07_ShowCurrentTime {
+    public static void main(String[] args) {
+        long totalSecond = System.currentTimeMillis() / 1000;
+    }
+}
+
+//                                                 _Output_
+/*
+
+ */
