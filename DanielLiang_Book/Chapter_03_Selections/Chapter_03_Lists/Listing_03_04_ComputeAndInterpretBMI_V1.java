237a56870e6cd924f245a7363cee70891661e2e9
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-07-29 23:57:14 +0300

    ComputeAndInterpretBMI_V1

---
+++ DanielLiang_Book/Chapter_03_Selections/Chapter_03_Lists/Listing_03_04_ComputeAndInterpretBMI_V1.java
@@ -0,0 +1,41 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 29/07/2024
+ * */
+
+package Chapter_03_Lists;
+
+import java.util.Scanner;
+
+public class Listing_03_04_ComputeAndInterpretBMI_V1 {
+    public static void main(String[] args) {
+        final double KILOGRAMS_PER_POUND = 0.45359237, METERS_PER_INCH = 0.0254;
+        double weightInPounds, heightInInches, weightInKilograms, heightInMeters, bmi;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter weight in pounds: ");
+        weightInPounds = input.nextDouble();
+        System.out.print("Enter height in inches: ");
+        heightInInches = input.nextDouble();
+        weightInKilograms = weightInPounds * KILOGRAMS_PER_POUND;
+        heightInMeters = heightInInches * METERS_PER_INCH;
+        bmi = weightInKilograms / Math.pow(heightInMeters, 2.0);
+        System.out.printf("BMI is %.15f\n", bmi);
+        if (bmi < 18.5)
+            System.out.print("Underweight");
+        else if (bmi < 25.0)
+            System.out.print("Normal");
+        else if (bmi < 30.0)
+            System.out.print("Overweight");
+        else
+            System.out.print("Obese");
+    }
+}
+
+//                                                 _Output_
+/*
+Enter weight in pounds: 146
+Enter height in inches: 70
+BMI is 20.948603801493316
+Normal
+ */
