6608e5dfd1c1187f256ca0bb6c59e7e9ef225d52
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-07-30 00:09:19 +0300

    ChineseZodiac

--- DanielLiang_Book/Chapter_03_Selections/Chapter_03_Lists/Listing_03_09_ChineseZodiac.java
+++ DanielLiang_Book/Chapter_03_Selections/Chapter_03_Lists/Listing_03_09_ChineseZodiac.java
@@ -6,11 +6,39 @@
 
 package Chapter_03_Lists;
 
-public class Listing_03_09_ChineseZodiac {
+import java.util.Scanner;
 
+public class Listing_03_09_ChineseZodiac {
+    public static void main(String[] args) {
+        int year;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        year = input.nextInt();
+        switch (year % 12) {
+            case 0:  System.out.print("monkey");break;
+            case 1:  System.out.print("rooster");break;
+            case 2:  System.out.print("dog");break;
+            case 3:  System.out.print("pig");break;
+            case 4:  System.out.print("rat");break;
+            case 5:  System.out.print("ox");break;
+            case 6:  System.out.print("tiger");break;
+            case 7:  System.out.print("rabbit");break;
+            case 8:  System.out.print("dragon");break;
+            case 9:  System.out.print("snake");break;
+            case 10: System.out.print("horse");break;
+            case 11: System.out.print("sheep");
+        }
+    }
 }
 
-//                                                 _Output_
+//                                                 _Output One_
 /*
+Enter a year: 1963
+rabbit
+ */
 
+//                                                 _Output Two_
+/*
+Enter a year: 1877
+ox
  */
