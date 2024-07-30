7ed20d37ca138f8fafa80ddd93c281ced0cc4822
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-07-30 17:52:24 +0300

    Random month

---
+++ DanielLiang_Book/Chapter_03_Selections/Chapter_03_Exercises/Exercise_03_04.java
@@ -0,0 +1,20 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 30/07/2024
+ * */
+
+package Chapter_03_Exercises;
+
+public class Exercise_03_04 {
+    public static void main(String[] args) {
+        final String[] MONTHS = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        int month = (int) (Math.random() * 12);
+        System.out.print(MONTHS[month]);
+    }
+}
+
+//                                                 _Output_
+/*
+November
+ */
