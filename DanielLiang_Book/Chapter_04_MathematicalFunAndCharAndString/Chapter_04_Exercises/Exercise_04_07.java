28db1c2457d7e04d0d249498a1bf31ee6a5222e6
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-08-24 01:58:06 +0300

    Corner point coordinates

---
+++ DanielLiang_Book/Chapter_04_MathematicalFunAndCharAndString/Chapter_04_Exercises/Exercise_04_07.java
@@ -0,0 +1,14 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 24/08/2024
+ * */
+
+package Chapter_04_Exercises;
+
+public class Exercise_04_07 {
+/*(Corner point coordinates) Suppose a pentagon is centered at (0, 0) with one point
+at the 0 o’clock position, as shown in Figure 4.7c. Write a program that prompts
+the user to enter the radius of the bounding circle of a pentagon and displays the
+coordinates of the five corner points on the pentagon.*/
+}
