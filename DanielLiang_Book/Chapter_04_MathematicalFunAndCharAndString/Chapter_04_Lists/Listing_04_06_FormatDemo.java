0c273e70c301c87c25d4ec2aec77e984c13f8022
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-08-23 20:55:05 +0300

    FormatDemo

---
+++ DanielLiang_Book/Chapter_04_MathematicalFunAndCharAndString/Chapter_04_Lists/Listing_04_06_FormatDemo.java
@@ -0,0 +1,19 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 23/08/2024
+ * */
+
+package Chapter_04_Lists;
+
+public class Listing_04_06_FormatDemo {
+    public static void main(String[] args) {
+        System.out.printf("%-10s%-10s%-10s%-10s%-10s\n", "Degree", "Radians", "Sine", "Cosine", "Tangent");
+        //System.out.printf("%-10d%.4f",30);
+    }
+}
+
+//                                                 _Output_
+/*
+
+ */
