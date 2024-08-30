00dab9cac4d1494a21bc790ec29ecfcc4a7ed2a1
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-08-30 16:46:00 +0300

    Random character

---
+++ DanielLiang_Book/Chapter_04_MathematicalFunAndCharAndString/Chapter_04_Exercises/Exercise_04_16.java
@@ -0,0 +1,18 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 30/08/2024
+ * */
+
+package Chapter_04_Exercises;
+
+public class Exercise_04_16 {
+    public static void main(String[] args) {
+        System.out.printf("The random character is '%c'",(char) ('A' + Math.random() * ('Z' - 'A' + 1)));
+    }
+}
+
+//                                                 _Output_
+/*
+The random character is 'M'
+ */
