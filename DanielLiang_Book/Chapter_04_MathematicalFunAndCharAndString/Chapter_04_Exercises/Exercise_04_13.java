881454e790e018f707ac14dcecbb7f981055fe69
Seif Yehia <57109944+SeifYehiaDev@users.noreply.github.com>
2024-08-30 16:45:21 +0300

    Vowel or consonant

---
+++ DanielLiang_Book/Chapter_04_MathematicalFunAndCharAndString/Chapter_04_Exercises/Exercise_04_13.java
@@ -0,0 +1,42 @@
+/*
+ * Author : Seif Yehia Esmail AbdelAal
+ * My accounts on social media (FACEBOOK , LINKEDIN , INSTAGRAM , GITHUB) : SeifYehiaDev
+ * Created on : 30/08/2024
+ * */
+
+package Chapter_04_Exercises;
+
+import java.util.Scanner;
+
+public class Exercise_04_13 {
+    public static void main(String[] args) {
+        char letter;
+        boolean isVowel;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a letter: ");
+        letter = input.nextLine().charAt(0);
+        if (Character.isLetter(letter)) {
+            isVowel = Character.toLowerCase(letter) == 'a' || Character.toLowerCase(letter) == 'e' || Character.toLowerCase(letter) == 'i' || Character.toLowerCase(letter) == 'o' || Character.toLowerCase(letter) == 'u';
+            System.out.printf("%c is a %s", letter, (isVowel) ? "vowel" : "consonant");
+        } else
+            System.out.printf("%c is an invalid input", letter);
+    }
+}
+
+//                                                 _Output One_
+/*
+Enter a letter: B
+B is a consonant
+ */
+
+//                                                 _Output Two_
+/*
+Enter a letter: a
+a is a vowel
+ */
+
+//                                                 _Output Three_
+/*
+Enter a letter: #
+# is an invalid input
+ */
